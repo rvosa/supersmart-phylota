8bdfc61baedd2eef45ef8c2f22bfbde4c184aaf0
rvosa <rutgeraldo@gmail.com>
2014-01-22 17:07:04 +0100

    nearly have treePL installation working

--- conf/manifests/default.pp
+++ conf/manifests/default.pp
@@ -30,9 +30,24 @@
 # 		command => "mysqladmin -uroot password $mysql_password",
 # 		require => Service["mysqld"],
 # 	}
+# vcsrepo { 
+# 	"/usr/local/src/supersmart":
+# 		ensure   => latest,
+# 		provider => git,
+# 		source   => 'https://github.com/naturalis/supersmart.git';
+# 
+# }
 
 Exec {
-	path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+	path => [ 
+		"/usr/local/sbin", 
+		"/usr/local/bin",
+		"/usr/sbin",
+		"/usr/bin",
+		"/sbin",
+		"/bin",
+		"/usr/lib64/openmpi/bin/",
+	]
 }
 
 package {
@@ -54,6 +69,11 @@ package {
 	"openmpi":                  ensure => installed;
 	"git":                      ensure => installed;
 	"openmpi-devel":            ensure => installed;
+	"zlib-devel":               ensure => installed;
+	"autoconf":                 ensure => installed;
+	"automake":                 ensure => installed;
+	"libtool":                  ensure => installed;
+	"gcc-c++":                  ensure => installed;	
 }
 
 file {
@@ -77,6 +97,11 @@ file {
 		ensure  => link,
 		target  => "/usr/local/src/ExaML/parser/parser",
 		require => Exec["compile_parser"];
+	"treepl_link":
+		path    => "/usr/local/bin/treePL",
+		ensure  => link,
+		target  => "/usr/local/src/treePL/src/treePL",
+		require => Exec["compile_treepl"];
 }
 
 exec {
@@ -98,14 +123,14 @@ exec {
 		command => "wget http://search.cpan.org/CPAN/authors/id/A/AJ/AJGOUGH/Parallel-MPI-Simple-0.10.tar.gz",
 		cwd     => "/usr/local/src",
 		creates => "/usr/local/src/Parallel-MPI-Simple-0.10.tar.gz",
-		require => Package[ 'wget', 'tar', 'openmpi' ];		
+		require => Package[ 'wget', 'tar', 'openmpi', 'openmpi-devel' ];		
 	"unzip_parallel_mpi_simple":
 		command => "tar -xzvf /usr/local/src/Parallel-MPI-Simple-0.10.tar.gz",
 		cwd     => "/usr/local/src",		
 		creates => "/usr/local/src/Parallel-MPI-Simple-0.10/Makefile.PL",
 		require => Exec["download_parallel_mpi_simple"];
 	"make_makefile_parallel_mpi_simple":
-		command => "perl Makefile.PL",
+		command => "perl Makefile.PL CCFLAGS='-I/usr/include/openmpi-x86_64/'",
 		cwd     => "/usr/local/src/Parallel-MPI-Simple-0.10/",
 		creates => "/usr/local/src/Parallel-MPI-Simple-0.10/Makefile",
 		require => Exec["unzip_parallel_mpi_simple"];
@@ -127,44 +152,62 @@ exec {
 		creates => "/usr/local/src/phylip-3.695/src/Makefile.unx",
 		require => Exec["download_phylip"];
 	"make_phylip":
-		command => "make -f Makefile.unx",
+		command => "make -f Makefile.unx consense",
 		cwd     => "/usr/local/src/phylip-3.695/src/",
 		creates => "/usr/local/src/phylip-3.695/src/consense",
 		require => Exec["unzip_phylip"];
 	
-	# clone examl & parser repo
+	# install examl & parser
 	"clone_examl":
 		command => "git clone https://github.com/stamatak/ExaML.git",
 		cwd     => "/usr/local/src",
 		creates => "/usr/local/src/ExaML/",
-		require => Package[ 'git' ];
-		
-	# compile examl
+		require => Package[ 'git', 'openmpi-devel', 'zlib-devel' ];
 	"compile_examl":
 		command => "make -f Makefile.SSE3.gcc",
 		cwd     => "/usr/local/src/ExaML/examl",
 		creates => "/usr/local/src/ExaML/examl/examl",
 		require => Exec["clone_examl"];
-	
-	# compile parser
 	"compile_parser":
 		command => "make -f Makefile.SSE3.gcc",
 		cwd     => "/usr/local/src/ExaML/parser",
 		creates => "/usr/local/src/ExaML/parser/parser",
 		require => Exec["clone_examl"];
 	
-	# clone treePL repo
+	# install treePL
 	"clone_treepl":
 		command => "git clone https://github.com/blackrim/treePL.git",
 		cwd     => "/usr/local/src",
 		creates => "/usr/local/src/treePL",
-		require => Package[ 'git' ];
+		require => Package[ 'git', 'autoconf', 'automake', 'libtool', 'gcc-c++', 'tar' ];
+	"unzip_adolc":
+		command => "tar -xvzf adol-c_git_saved.tar.gz",
+		cwd     => "/usr/local/src/treePL/deps",
+		creates => "/usr/local/src/treePL/deps/adol-c",
+		require => Exec["clone_treepl"];
+	"autoreconf_adolc":
+		command => "autoreconf -fi",
+		cwd     => "/usr/local/src/treePL/deps/adol-c",
+		creates => "/usr/local/src/treePL/deps/adol-c/configure",
+		require => Exec["unzip_adolc"];
+	"install_adolc":
+		command => "/usr/local/src/treePL/deps/adol-c/configure --prefix=/usr --with-openmp-flag=-fopenmp && make install",
+		cwd     => "/usr/local/src/treePL/deps/adol-c",
+		creates => "/usr/lib64/libadolc.so",
+		require => Exec["autoreconf_adolc"];
+	"unzip_nlopt":
+		command => "tar -xzvf nlopt-2.2.4.tar.gz",
+		cwd     => "/usr/local/src/treePL/deps",
+		creates => "/usr/local/src/treePL/deps/nlopt-2.2.4",
+		require => Exec["clone_treepl"];
+	"install_nlopt":
+		command => "/usr/local/src/treePL/deps/nlopt-2.2.4/configure && make install",
+		cwd		=> "/usr/local/src/treePL/deps/nlopt-2.2.4",
+		creates => "/usr/local/include/nlopt.h",
+		require => Exec["unzip_nlopt"];
+	"compile_treepl":
+		command => "/usr/local/src/treePL/src/configure && make",
+		cwd     => "/usr/local/src/treePL/src",
+		creates => "/usr/local/src/treePL/src/treePL",
+		require => Exec["install_adolc","install_nlopt"];		
 }
-
-# vcsrepo { 
-# 	"/usr/local/src/supersmart":
-# 		ensure   => latest,
-# 		provider => git,
-# 		source   => 'https://github.com/naturalis/supersmart.git';
-# 
-# }
