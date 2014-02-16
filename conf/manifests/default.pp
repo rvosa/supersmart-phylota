e38507caf1fefda0a7295f3abf0f6a87260b4376
Rutger Vos <rutgeraldo@gmail.com>
2014-02-16 12:54:38 +0100

    now sets all perl search paths, installs bioperl-* from github, grants all to mysql on phylota.*

--- conf/manifests/default.pp
+++ conf/manifests/default.pp
@@ -28,8 +28,7 @@ package {
 	"perl-XML-Twig":            ensure => installed;
 	"perl-HTML-Parser":         ensure => installed;
 	"perl-Config-Tiny":         ensure => installed;
-	"perl-bioperl":             ensure => installed; # XXX we will need to do this by hand
-	"perl-bioperl-run":         ensure => installed; # XXX we will need to do this by hand
+	"perl-IO-String":           ensure => installed;
 	"git":                      ensure => installed;
 	"zlib-devel":               ensure => installed;
 	"autoconf":                 ensure => installed;
@@ -86,19 +85,13 @@ file {
 # command line tasks
 exec {
 
-	# download data	
+	# make phylota database
 	"dl_phylota_dump":
 		command => "wget https://dl.dropboxusercontent.com/u/4180059/phylota.tar.gz",
 		cwd     => "/usr/share/supersmart",
 		creates => "/usr/share/supersmart/phylota.tar.gz",
 		timeout => 0,
 		require => [ File[ 'data_dir' ], Package[ 'wget' ] ];
-	"dl_inparanoid_seq":
-		command => "curl http://inparanoid.sbc.su.se/download/current/sequences/processed/FASTA -o inparanoid.fa",
-		cwd     => "/usr/share/supersmart",
-		creates => "/usr/share/supersmart/inparanoid.fa",
-		timeout => 0,		
-		require => [ File[ 'data_dir' ], Package[ 'curl' ] ];
 	"unzip_phylota_dump":
  		command => "tar -xzvf phylota.tar.gz",
  		creates => "/usr/share/supersmart/phylota",
@@ -114,6 +107,9 @@ exec {
  		command => "chown -R -h mysql:mysql phylota/",
  		cwd     => "/var/lib/mysql/",
  		require => Exec[ 'symlink_phylota_dump' ];
+ 	"grant_phylota_db":
+ 		command => "mysql -u root -e \"grant all on phylota.* to 'mysql'@'localhost';\"",
+ 		require => Exec[ 'chown_phylota_db' ];
  		
  	# install mafft
  	"dl_mafft":
@@ -133,22 +129,18 @@ exec {
  		creates => "/usr/local/bin/mafft"; 		
 
 	# make inparanoid blast db
+	"dl_inparanoid_seq":
+		command => "curl http://inparanoid.sbc.su.se/download/current/sequences/processed/FASTA -o inparanoid.fa",
+		cwd     => "/usr/share/supersmart",
+		creates => "/usr/share/supersmart/inparanoid.fa",
+		timeout => 0,		
+		require => [ File[ 'data_dir' ], Package[ 'curl' ] ];	
 	"inparanoid_formatdb":
 		command => "makeblastdb -in inparanoid.fa -logfile inparanoid-blast.log -dbtype prot -parse_seqids",
 		cwd     => "/usr/share/supersmart",
 		creates => "/usr/share/supersmart/inparanoid-blast.log",
 		timeout => 0,
 		require => [ Exec[ 'dl_inparanoid_seq' ], Package[ 'ncbi-blast+.x86_64' ] ];		
-
-	# make profile.d files
-	"make_supersmart_sh":
-		command => "echo 'export LD_LIBRARY_PATH=/usr/lib' > supersmart.sh && echo 'export SUPERSMART_HOME=/usr/local/src/supersmart' >> supersmart.sh",
-		cwd     => "/etc/profile.d",
-		creates => "/etc/profile.d/supersmart.sh";
-	"make_supersmart_csh":
-		command => "echo 'setenv LD_LIBRARY_PATH /usr/lib' > supersmart.csh && echo 'setenv SUPERSMART_HOME /usr/local/src/supersmart' >> supersmart.csh",
-		cwd     => "/etc/profile.d",
-		creates => "/etc/profile.d/supersmart.csh";
 
 	# install muscle multiple sequence alignment
 	"download_muscle":
@@ -162,27 +154,57 @@ exec {
 		creates => "/usr/local/src/muscle3.8.31_i86linux64",
 		require => Exec["download_muscle"];
 
-    # install perl package Bio::Phylo
-	"download_bio_phylo":
-		command => "wget http://search.cpan.org/CPAN/authors/id/R/RV/RVOSA/Bio-Phylo-0.56.tar.gz",
-		cwd => "/usr/local/src",
-		creates => "/usr/local/src/Bio-Phylo-0.56.tar.gz",
-		require => Package[ 'wget', 'tar' ];
-	"unzip_bio_phylo":
-		command => "tar -xzvf /usr/local/src/Bio-Phylo-0.56.tar.gz",
+	# install Bio::Phylo
+	"clone_bio_phylo":
+		command => "git clone https://github.com/rvosa/bio-phylo.git",
 		cwd     => "/usr/local/src",
-		creates => "/usr/local/src/Bio-Phylo-0.56/Makefile.PL",
-		require => Exec["download_bio_phylo"];
-	"make_makefile_bio_phylo":
-		command => "perl Makefile.PL",
-		cwd     => "/usr/local/src/Bio-Phylo-0.56/",
-		creates => "/usr/local/src/Bio-Phylo-0.56/Makefile",
-		require => Exec["unzip_bio_phylo"];
-	"make_install_bio_phylo":
-		command => "make install",
-		cwd     => "/usr/local/src/Bio-Phylo-0.56/",		
-		creates => "/usr/local/share/perl5/Bio/Phylo.pm",
-		require => Exec["make_makefile_bio_phylo"];	
+		creates => "/usr/local/src/bio-phylo",
+		require => Package[ 'git' ];
+	"make_bio_phylo_sh":
+		command => "echo 'export PERL5LIB=\$PERL5LIB:/usr/local/src/bio-phylo/lib' > biophylo.sh",
+		cwd     => "/etc/profile.d",
+		creates => "/etc/profile.d/biophylo.sh",
+		require => Exec[ 'clone_bio_phylo' ];
+	"make_bio_phylo_csh":
+		command => "echo 'setenv PERL5LIB \$PERL5LIB:/usr/local/src/bio-phylo/lib' > biophylo.csh",
+		cwd     => "/etc/profile.d",
+		creates => "/etc/profile.d/biophylo.csh",
+		require => Exec[ 'clone_bio_phylo' ];
+	
+	# install bioperl-live
+	"clone_bioperl_live":
+		command => "git clone -b v1.6x https://github.com/bioperl/bioperl-live.git",
+		cwd     => "/usr/local/src",
+		creates => "/usr/local/src/bioperl-live",
+		require => Package[ 'git' ];		
+	"make_bioperl_live_sh":
+		command => "echo 'export PERL5LIB=\$PERL5LIB:/usr/local/src/bioperl-live' > bioperl_live.sh",
+		cwd     => "/etc/profile.d",
+		creates => "/etc/profile.d/bioperl_live.sh",
+		require => Exec[ 'clone_bioperl_live' ];
+	"make_bioperl_live_csh":
+		command => "echo 'setenv PERL5LIB \$PERL5LIB:/usr/local/src/bioperl-live' > bioperl_live.csh",
+		cwd     => "/etc/profile.d",
+		creates => "/etc/profile.d/bioperl_live.csh",
+		require => Exec[ 'clone_bioperl_live' ];		
+
+	# install bioperl-run
+	"clone_bioperl_run":
+		command => "git clone https://github.com/bioperl/bioperl-run.git",
+		cwd     => "/usr/local/src",
+		creates => "/usr/local/src/bioperl-run",
+		require => Package[ 'git' ];		
+	"make_bioperl_run_sh":
+		command => "echo 'export PERL5LIB=\$PERL5LIB:/usr/local/src/bioperl-run/lib' > bioperl_run.sh",
+		cwd     => "/etc/profile.d",
+		creates => "/etc/profile.d/bioperl_run.sh",
+		require => Exec[ 'clone_bioperl_run' ];
+	"make_bioperl_run_csh":
+		command => "echo 'setenv PERL5LIB \$PERL5LIB:/usr/local/src/bioperl-run/lib' > bioperl_run.csh",
+		cwd     => "/etc/profile.d",
+		creates => "/etc/profile.d/bioperl_run.csh",
+		require => Exec[ 'clone_bioperl_run' ];	
+		
 
     # install perl package Parallel::MPI::Simple
 	"download_parallel_mpi_simple":
@@ -264,6 +286,14 @@ exec {
 		cwd     => "/usr/local/src",
 		creates => "/usr/local/src/supersmart",
 		require => Package[ 'git' ];
+	"make_supersmart_sh":
+		command => "echo 'export LD_LIBRARY_PATH=/usr/lib' > supersmart.sh && echo 'export SUPERSMART_HOME=/usr/local/src/supersmart' >> supersmart.sh && echo 'export PERL5LIB=\$PERL5LIB:\$SUPERSMART_HOME/lib' >> supersmart.sh",
+		cwd     => "/etc/profile.d",
+		creates => "/etc/profile.d/supersmart.sh";
+	"make_supersmart_csh":
+		command => "echo 'setenv LD_LIBRARY_PATH /usr/lib' > supersmart.csh && echo 'setenv SUPERSMART_HOME /usr/local/src/supersmart' >> supersmart.csh && echo 'setenv PERL5LIB \$PERL5LIB:\$SUPERSMART_HOME/lib' >> supersmart.csh",
+		cwd     => "/etc/profile.d",
+		creates => "/etc/profile.d/supersmart.csh";		
 	
 	# install treePL
 	"clone_treepl":
