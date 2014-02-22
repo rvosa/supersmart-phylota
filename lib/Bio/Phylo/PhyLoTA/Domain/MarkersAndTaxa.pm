4e9179607587d18c766551ee644b42717ece5296
Rutger Vos <rutgeraldo@gmail.com>
2014-02-22 14:40:40 -0700

    added taxon table methods, and some more alignment methods

--- lib/Bio/Phylo/PhyLoTA/Domain/MarkersAndTaxa.pm
+++ lib/Bio/Phylo/PhyLoTA/Domain/MarkersAndTaxa.pm
@@ -72,6 +72,72 @@ sub parse_fasta_string {
     return %fasta;    
 }
 
+=item parse_taxa_file
+
+Reads the provided taxa file, returns a list of records where each consists of a 
+hash where keys are the headers in the taxa table.
+
+=cut
+
+sub parse_taxa_file {
+	my ( $class, $file ) = @_;
+	open my $fh, '<', $file or die $!;
+	my $string = do { local $/; <$fh> };
+	return $class->parse_taxa_string($string);
+	
+}
+
+=item parse_taxa_string
+
+Reads the provided taxa string, returns a list of records where each consists of a 
+hash where keys are the headers in the taxa table.
+
+=cut
+
+sub parse_taxa_string {
+	my ( $class, $string ) = @_;
+	my @header;
+	my @result;
+	LINE: for my $line ( split /\n/, $string ) {
+		chomp $line;
+		if ( not @header ) {
+			@header = split /\t/, $line;
+			next LINE;
+		}
+		my @fields = split /\t/, $line;
+		my %record;
+		for my $i ( 0 .. $#header ) {
+			$record{$header[$i]} = $fields[$i];
+		}
+		push @result, \%record;
+	}
+	return @result;
+}
+
+=item get_distinct_taxa
+
+Returns the distinct IDs for the provided taxonomic level.
+
+=cut
+
+sub get_distinct_taxa {
+	my ( $class, $taxon, @records ) = @_;
+	my %ids = map { $_ => 1 } map { $_->{$taxon} } @records;
+	return keys %ids;
+}
+
+=item get_species_for_taxon
+
+Returns the distinct species IDs for the provided taxon ID.
+
+=cut
+
+sub get_species_for_taxon {
+	my ( $class, $taxon, $id, @records ) = @_;
+	my %ids = map { $_ => 1 } map { $_->{'species'} } grep { $_->{$taxon} == $id } @records;
+	return keys %ids;
+}
+
 =item get_taxa_from_fasta
 
 Extracts taxon identifiers from a hash such as is produced by parse_fasta_string
@@ -88,6 +154,36 @@ sub get_taxa_from_fasta {
         }
     }
     return @taxa;
+}
+
+=item get_sequences_for_taxon
+
+Extracts a list of sequence strings for the provided taxon from a hash such as is
+produced by parse_fasta_string
+
+=cut
+
+sub get_sequences_for_taxon {
+	my ( $class, $taxon, %fasta ) = @_;
+	my @sequences;
+	for my $key ( keys %fasta ) {
+		if ( $key =~ /taxon\|$taxon[^\d]/ ) {
+			push @sequences, $fasta{$key};
+		}
+	}
+	return @sequences;
+}
+
+=item get_nchar
+
+Returns the number of characters in the fasta hash.
+
+=cut
+
+sub get_nchar {
+	my ( $class, %fasta ) = @_;
+	my ($seq) = values %fasta;
+	return length $seq;
 }
 
 =item keep_taxa
