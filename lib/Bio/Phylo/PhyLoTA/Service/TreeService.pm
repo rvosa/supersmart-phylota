61f0c7cf8d42fc91720be352e02c74ace834b924
Rutger Vos <rutgeraldo@gmail.com>
2014-02-22 14:40:02 -0700

    adding stubs for a future tree service

---
+++ lib/Bio/Phylo/PhyLoTA/Service/TreeService.pm
@@ -0,0 +1,34 @@
+package Bio::Phylo::PhyLoTA::Service::TreeService;
+use strict;
+use warnings;
+use File::Temp 'tempfile';
+use Bio::Phylo::PhyLoTA::Config;
+
+my $config = Bio::Phylo::PhyLoTA::Config->new;
+
+# writes a tree description that spans provided taxa/clade to file
+sub write_tree {
+
+}
+
+# -infile, -format, -outfile, -burnin
+sub consense_trees {
+	my ( $self, %args ) = @_;
+
+}
+
+# splices a clade tree into a backbone tree
+sub graft_trees {
+
+}
+
+# builds a tree using the configured tree inference method
+sub infer_backbone_tree {
+
+}
+
+sub infer_clade_tree {
+
+}
+
+1;
