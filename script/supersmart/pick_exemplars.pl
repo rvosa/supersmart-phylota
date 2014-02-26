5257b29db6ade0a2d224063ad90f2e65c1b0fa97
Rutger Vos <rutgeraldo@gmail.com>
2014-02-26 01:34:19 +0100

    now picks exemplars based on the frequency of their occupying most distal rank, scaled by the data set size. roughly optimally packs these taxa in a minimum of alignments

--- script/supersmart/pick_exemplars.pl
+++ script/supersmart/pick_exemplars.pl
@@ -2,8 +2,11 @@
 use strict;
 use warnings;
 use Getopt::Long;
-use List::Util qw(min max);
+use Data::Dumper;
+use File::Temp 'tempfile';
+use List::Util qw(min max sum);
 use Bio::Phylo::Matrices::Datum;
+use Bio::Phylo::PhyLoTA::Config;
 use Bio::Phylo::Util::Logger ':levels';
 use Bio::Phylo::PhyLoTA::Domain::MarkersAndTaxa;
 
@@ -19,8 +22,9 @@ pick_exemplars.pl - selects per-genus exemplar taxa from a set of alignments
 
 Given an input file that lists alignments (one on each line), traverses each genus in
 each alignment and picks the overall most divergent two species to represent their
-genus. The rationale is that these species will cross the root of their genus, so that
-the genus-level tree can then be scaled to the same depth and grafted onto the tree.
+genus. The rationale is that these species will (likely) cross the root of their genus, 
+so that the genus-level tree can then be scaled to the same depth of that split and 
+be grafted onto the tree without (too many) negative branch lengths.
 
 B<Point of consideration>: the node depths on the exemplar tree will be underestimates
 relative to the genus-level tree (due to the node density effect), so it might be better
@@ -40,6 +44,8 @@ GetOptions(
 );
 
 # instantiate helper objects
+my $config = Bio::Phylo::PhyLoTA::Config->new;
+my $mt = Bio::Phylo::PhyLoTA::Domain::MarkersAndTaxa->new;
 my $log = Bio::Phylo::Util::Logger->new(
 	'-class' => 'main',
 	'-level' => $verbosity,
@@ -52,12 +58,12 @@ my @alignments;
 	open my $fh, '<', $list or die $!;
 	while(<$fh>) {
 		chomp;
-		push @alignments, $_;
+		push @alignments, $_ if /\S/ && -e $_;
 	}
 }
 
 # read taxa table
-my $mt = Bio::Phylo::PhyLoTA::Domain::MarkersAndTaxa->new;
+$log->info("going to read taxa table $taxa");
 my @records = $mt->parse_taxa_file($taxa);
 
 # extract and iterate over the distinct genera
@@ -72,14 +78,12 @@ for my $genus ( $mt->get_distinct_taxa( 'genus' => @records ) ) {
 # here comes the convoluted logic:
 # 1. for each alignment, calculate all pairwise distances within each genus
 # 2. if multiple sequences for the same species, calculate the average distance
-# 3. normalize the distances, e.g. between 0 and 1. This is so that we don't pick
-#    as exemplar pair two taxa that just happened to be divergent in a rapidly
-#    evolving locus even though they are not the overall most divergent
-# 4. sort the distances, retaining the most distal pair for each alignment
-# 5. pick the modal pair across alignments
+# 3. sort the pairs by distance within each alignment
+# 4. assign the most distal pair a score that is proportional to the size of the sample
 my %pairs;
 my $dat = 'Bio::Phylo::Matrices::Datum';
-for my $aln ( @alignments ) {
+ALN: for my $aln ( @alignments ) {
+	$log->info("assessing exemplars in $aln");
 	my %fasta = $mt->parse_fasta_file($aln);
 	GENUS: for my $genus ( keys %genera ) {
 		next GENUS if @{ $genera{$genus} } < 3;	
@@ -87,23 +91,35 @@ for my $aln ( @alignments ) {
 	
 		# lump instantiated sequence objects by species within this genus
 		my %seq;
+		my $sequence_count = 0;
 		for my $species ( @{ $genera{$genus} } ) {
 			if ( my @raw = $mt->get_sequences_for_taxon( $species, %fasta ) ) {
-				$log->info("$aln contained ".scalar(@raw)." sequences for $species");
+				$log->debug("$aln contained ".scalar(@raw)." sequences for species $species");
 				my @seq = map { $dat->new( '-type' => 'dna', '-char' => $_ ) } @raw;
 				$seq{$species} = \@seq;
+				$sequence_count++;
 			}
-			$log->info("$aln has no sequences for $species");
+			else {
+				$log->debug("$aln has no sequences for species $species");
+			}
+		}
+		if ( $sequence_count == 0 ) {
+			$log->debug("$aln has no sequences for genus $genus");
+			next GENUS;
 		}
 		
 		# calculate the distances within the genus
 		my %dist;
 		my @species = keys %seq;
+		if ( scalar(@species) < 2 ) {
+			$log->debug("not enough species for genus $genus in $aln");
+			next GENUS;
+		}
 		for my $i ( 0 .. ( $#species - 1 ) ) {
 			for my $j ( ( $i + 1 ) .. $#species ) {
 				my $sp1 = $species[$i];
 				my $sp2 = $species[$j];
-				$log->info("going to compute average distance between $sp1 and $sp2");
+				$log->debug("going to compute average distance between $sp1 and $sp2");
 				my @seqs1 = @{ $seq{$sp1} };
 				my @seqs2 = @{ $seq{$sp2} };
 				my $dist;
@@ -118,17 +134,10 @@ for my $aln ( @alignments ) {
 			}
 		}
 		
-		# normalize the distances
-		my $min = min values %dist;
-		my $max = max values %dist;
-		my $scale = 1 / ( $max - $min ); # XXX can divide by 0?
-		for my $pair ( keys %dist ) {
-			$dist{$pair} = ( $dist{$pair} - $min ) * $scale;
-		}
-		
 		# pick the most distal pair, increment its occurrence
 		my ($farthest) = sort { $dist{$b} <=> $dist{$a} } keys %dist;
-		$pairs{$genus}->{$farthest}++;
+		$pairs{$genus}->{$farthest} += sum( 0 .. (scalar(keys(%dist))-1) );
+		$log->debug("most distal pair for genus $genus in $aln: $farthest");
 	}
 }
 
@@ -144,29 +153,69 @@ for my $genus ( keys %pairs ) {
 		my %p = %{ $pairs{$genus} };
 		my ($sp1,$sp2) = map { split /\|/, $_ } sort { $p{$b} <=> $p{$a} } keys %p;
 		push @exemplars, $sp1, $sp2;
+		$log->debug(Dumper({ $genus => \%p }));
 	}
 }
 
-# make the output
-my %supermatrix;
-for my $taxon ( @exemplars ) {
-	$supermatrix{$taxon} = '';
-	for my $aln ( @alignments ) {
-		my %fasta = $mt->parse_fasta_file($aln);
-		my $nchar = $mt->get_nchar(%fasta);
+# make the best set of alignments:
+# 1. map exemplar taxa to alignments and vice versa
+my ( %taxa_for_aln, %alns_for_taxon, %nchar );
+for my $aln ( @alignments ) {
+	$taxa_for_aln{$aln} = [] if not $taxa_for_aln{$aln};
+	my %fasta = $mt->parse_fasta_file($aln);
+	$nchar{$aln} = $mt->get_nchar(%fasta);
+	for my $taxon ( @exemplars ) {
+		$alns_for_taxon{$taxon} = [] if not $alns_for_taxon{$taxon};	
 		my ($seq) = $mt->get_sequences_for_taxon($taxon,%fasta);
 		if ( $seq ) {
-			$supermatrix{$taxon} .= $seq;
-		}
-		else {
-			$supermatrix{$taxon} .= '?' x $nchar;
+			push @{ $taxa_for_aln{$aln} }, $taxon;
+			push @{ $alns_for_taxon{$taxon} }, $aln;
 		}
 	}
 }
-
-# print the output
-for my $taxon ( keys %supermatrix ) {
-	if ( $supermatrix{$taxon} !~ /^\?+$/ ) {
-		print '>', $taxon, "\n", $supermatrix{$taxon}, "\n";
+# 2. for each taxon, sort its alignments from more to less inclusive
+for my $taxon ( @exemplars ) {
+	my @alignments = @{ $alns_for_taxon{$taxon} };
+	my @sorted = sort { scalar(@{$taxa_for_aln{$b}}) <=> scalar(@{$taxa_for_aln{$a}}) } @alignments;
+	$alns_for_taxon{$taxon} = \@alignments;
+}
+# 3. sort the taxa from less to more often encountered
+my @sorted_exemplars = sort { scalar(@{$alns_for_taxon{$a}}) <=> scalar(@{$alns_for_taxon{$b}}) } @exemplars;
+my %aln;
+my %seen;
+TAXON: for my $taxon ( @sorted_exemplars ) {
+	if ( $alns_for_taxon{$taxon} ) {
+		my $aln = shift @{ $alns_for_taxon{$taxon} };
+		$aln{$aln}++;
+		for my $other_taxa ( @{ $taxa_for_aln{$aln} } ) {
+			delete $alns_for_taxon{$other_taxa} if ++$seen{$other_taxa} == $config->BACKBONE_MIN_COVERAGE;
+		}
 	}
+	last TAXON unless scalar keys %alns_for_taxon;
+}
+my @filtered_exemplars = grep { ! $alns_for_taxon{$_} } @sorted_exemplars;
+
+# produce interleaved phylip output
+my @sorted_alignments = keys %aln;
+my $nchar = sum( @nchar{@sorted_alignments} );
+my $ntax  = scalar(@filtered_exemplars);
+my $names_printed;
+print $ntax, ' ', $nchar, "\n";
+for my $aln ( @sorted_alignments ) {
+	$log->info("including $aln in supermatrix");
+	my %fasta = $mt->parse_fasta_file($aln);
+	for my $taxon ( @filtered_exemplars ) {
+		if ( not $names_printed ) {
+			print $taxon, ' ' x ( 10 - length($taxon) );
+		}
+		my ( $seq ) = $mt->get_sequences_for_taxon($taxon,%fasta);
+		if ( $seq ) {
+			print $seq;
+		}
+		else {
+			print '?' x $nchar{$aln};
+		}
+		print "\n";
+	}
+	$names_printed++;
 }
