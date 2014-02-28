476e48c83e00eb0764f70e8cca9dd70f1c6bd853
rvosa <rutgeraldo@gmail.com>
2014-02-28 09:55:18 +0100

    added more documentation

--- script/supersmart/remap.pl
+++ script/supersmart/remap.pl
@@ -4,6 +4,21 @@ use warnings;
 use Bio::Phylo::IO 'parse_tree';
 use Bio::Phylo::PhyLoTA::Service;
 
+=head1 NAME
+
+remap.pl - re-maps NCBI taxon identifiers as leaf labels back to scientific binomials
+
+=head1 SYNOPSYS
+
+ $ perl remap.pl <infile> > <outfile>
+
+=head1 DESCRIPTION
+
+Given an input file in Newick syntax, parses the file and traverses the tree to map
+the NCBI taxon IDs that are used for leaf labels back to scientific names.
+
+=cut
+
 my $infile = shift;
 my $service = Bio::Phylo::PhyLoTA::Service->new;
 
