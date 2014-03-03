cdf6a735907f12870d49a62312f56f909388a5de
Rutger Vos <rutgeraldo@gmail.com>
2014-03-03 16:50:23 +0100

    refactored create_calibration_table

--- lib/Bio/Phylo/PhyLoTA/Service/CalibrationService.pm
+++ lib/Bio/Phylo/PhyLoTA/Service/CalibrationService.pm
@@ -1,6 +1,7 @@
 package Bio::Phylo::PhyLoTA::Service::CalibrationService;
 use strict;
 use warnings;
+use File::Temp 'tempfile';
 use Bio::Phylo::PhyLoTA::Service;
 use Bio::Phylo::PhyLoTA::Domain::FossilData;
 use Bio::Phylo::PhyLoTA::Domain::CalibrationTable;
@@ -26,7 +27,7 @@ Bio::Phylo::PhyLoTA::Service::CalibrationService - manages tree calibration
  my @identified = map { $cs->find_calibration_point($_) } @fossils;
  
  # read a newick tree
- my $tree = parse(
+ my $tree = parse_tree(
  	'-format' => 'newick',
  	'-file'   => 'tree.dnd',
  	'-as_project' => 1,
@@ -36,11 +37,19 @@ Bio::Phylo::PhyLoTA::Service::CalibrationService - manages tree calibration
  my $ct = $cs->create_calibration_table( $tree, @identified );
  
  # calibrate the tree
- my $chronogram = $cs->calibrate_tree($tree,$ct);
+ my $chronogram = $cs->calibrate_tree(
+ 	'-tree'              => $tree,
+ 	'-numsites'          => 1140,
+ 	'-calibration_table' => $ct,
+ );
 
 =head1 DESCRIPTION
 
-XXX
+This service class manages the workflow with respect to tree calibration. This includes
+reading of PROTEUS-compatible fossil data sets ('read_fossil_table'), identifying the
+corresponding taxa they represent ('find_calibration_points'), turning these into a
+calibration table that represents the relevant calibration points given the focal tree
+('create_calibration_table') and finally using these to produce a chronogram.
 
 =head1 METHODS
 
@@ -56,6 +65,38 @@ sub new {
     my $class = shift;
     my $self = bless {}, $class;
     return $self;
+}
+
+=item calibrate_tree
+
+=cut
+
+sub calibrate_tree {
+	my ($self,%args) = @_;
+	my $smooth    = $args{'-treepl_smooth'}     || $config->TREEPL_SMOOTH;
+	my $numsites  = $args{'-numsites'}          || die "Need -numsites argument";
+	my $ct        = $args{'-calibration_table'} || die "Need -calibration_table argument";
+	my $tree      = $args{'-tree'}              || die "Need -tree argument";
+	my $nthreads  = $config->NODES;
+	my ($ifh,$writetree) = tempfile();
+	my ($ofh,$readtree)  = tempfile();
+	my ($tfh,$tplfile)   = tempfile();
+	print $ofh $tree->to_newick;
+	close $ofh;
+	close $ifh;
+	
+	# print treePL config file header
+	print $tfh <<"HEADER";
+treefile = $readtree
+smooth = $smooth
+numsites = $numsites
+outfile = $writetree
+nthreads = $nthreads
+HEADER
+
+	# print MRCA statements
+	print $tfh $ct->to_string, "thorough\n";
+
 }
 
 =item find_calibration_point
@@ -78,9 +119,10 @@ sub find_calibration_point {
     
     # if nodes are found, attach it as a calibration point 
     # to the FossilData object
-    $log->info("found ".scalar(@nodes)." nodes for fossil ".$fd->fossil_name);
+    $log->debug("found ".scalar(@nodes)." nodes for fossil ".$fd->fossil_name);
     if ( scalar @nodes > 0 ){
 		$fd->calibration_points(@nodes);
+		$log->info($_->taxon_name) for @nodes;
     }
     return $fd;
 }
@@ -98,56 +140,85 @@ sub create_calibration_table {
     my ( $self, $tree, @fossildata ) = @_;
     my $config = $self->config;
     my $logger = $self->logger;        
-    my $table = Bio::Phylo::PhyLoTA::Domain::CalibrationTable->new;
+    my $table  = Bio::Phylo::PhyLoTA::Domain::CalibrationTable->new;
     my $cutoff = $config->FOSSIL_BEST_PRACTICE_CUTOFF;
+    my $mts    = Bio::Phylo::PhyLoTA::Service::MarkersAndTaxaSelector->new;
     
-    # iterate over fossils
-    for my $fd ( @fossildata ) {	
-		my @nodes = $fd->calibration_points();
+    # created mapping from ti to fossil
+    $logger->info("going to map fossils to calibrated taxon IDs");
+    my %fd_for_id;
+    for my $fd ( @fossildata ) {
+		my @nodes = $fd->calibration_points; # returns higher taxa
 		my $score = $fd->best_practice_score || 0;
-	    if ($score >= $cutoff){	    
-			if ( scalar @nodes > 0) {
-			
-				# fetch all the tips in the taxonomy using a level-order,
-				# non-recursive, queue-based traversal
-				my @tips;
-				my @queue = @nodes;
-				while ( @queue ) {
-					my $node = shift @queue;
-					my @children = @{ $node->get_children };
-					if ( @children ) {
-						push @queue, @children;
+		if ( $score >= $cutoff ) {
+			if ( scalar @nodes > 0 ) {
+				for my $n ( @nodes ) {
+					my $id = $n->ti;
+					$fd_for_id{$id} = { 'fd' => $fd };
+					
+					# create mapping to descendant nodes
+					my %seen;
+					my @queue = @{ $n->get_children };
+					while(@queue) {
+						my $child = shift @queue;
+						$seen{$child->ti}++;
+						my @children = @{ $child->get_children };
+						push @queue, @children if @children;
 					}
-					else {						    
-						my $name = $node->get_name;	
-								
-						# caution! assuming underscore-convention for 
-						# whitespaces in taxon names in the tree
-						$name =~s/\ /\_/g;	
-						
-						# search for the equivalent node in the tree
-						if ( my $tip = $tree->get_by_name($name) ) {
-							push @tips, $tip;
-						}
-					}
+					$fd_for_id{$id}->{'desc'} = \%seen;			
 				}
-				if (scalar @tips > 0){
-				    
-					# get most recent common ancestor
-					my $mrca = $tree->get_mrca(\@tips);
-			
-					# create record in table
-					$table->add_row(
-						'taxa'    => [ map { $_->get_name } @{ $mrca->get_terminals } ],
-						'min_age' => $fd->min_age,			    
-					);
-				}
-			}	    
-	    }
+			}
+		} 
 		else {
 			$logger->info("Best practice score for fossil record for taxon"
 				  .$fd->calibrated_taxon()." too low.");
+		}		   	
+    }
+    
+    # assign each tip to the most recent (i.e. least inclusive) calibrated
+    # mrca that subtends it
+    $logger->info("going to assign tree leaves to calibrated ancestral taxa");
+    my %tips_for_id = map { $_ => [] } keys %fd_for_id;
+    my @sorted = sort { scalar(keys(%{$fd_for_id{$a}->{'desc'}})) <=> scalar(keys(%{$fd_for_id{$b}->{'desc'}})) } keys %fd_for_id;
+    for my $tip ( @{ $tree->get_terminals } ) {
+    	my $name = $tip->get_name;
+    	$logger->debug("trying to assign leaf $name...");
+    	my $node;
+    	if ( $name =~ /^\d+$/ ) {
+    		$node = $mts->find_node($name);
+    	}
+    	else {
+    		$name =~ s/_/ /g;
+    		($node) = $mts->get_nodes_for_names($name);
+    	}
+    	my $id = $node->ti;
+		for my $fid ( @sorted ) {
+			if ( $fd_for_id{$fid}->{'desc'}->{$id} ) {
+				push @{ $tips_for_id{$fid} }, $tip;
+				my $ct = $fd_for_id{$fid}->{'fd'}->calibrated_taxon;
+				$logger->debug("assigning leaf $name to $ct");
+			}
 		}
+    }
+    
+    # add the fossils that we could place on the tree to the table
+    $logger->info("going to add within-clade calibrated taxa in table");
+    for my $id ( keys %tips_for_id ) {
+    	if ( scalar @{ $tips_for_id{$id} } ) {
+    	
+			# create record in table
+			$table->add_row(
+				'taxa'    => [ sort { $a cmp $b } map { $_->get_name } @{ $tips_for_id{$id} } ],
+				'min_age' => $fd_for_id{$id}->{'fd'}->min_age,
+				'name'    => $fd_for_id{$id}->{'fd'}->calibrated_taxon,	    
+			);    	
+    	}
+    	else {
+    	
+    		# this can happen, for example, if the CalibratedTaxon was a 
+    		# homonym in a different kingdom
+    		$logger->warn("could not find descendants for id $id in focal tree");
+    	}    
     }
     return $table;
 }
