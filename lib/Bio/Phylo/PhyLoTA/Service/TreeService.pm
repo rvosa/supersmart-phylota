2f1a87060b608f7dae04f637915e246e6d33ebf6
hettling <hannes.hettling@gmail.com>
2014-04-15 17:38:21 +0200

    added tree rooting

--- lib/Bio/Phylo/PhyLoTA/Service/TreeService.pm
+++ lib/Bio/Phylo/PhyLoTA/Service/TreeService.pm
@@ -7,6 +7,8 @@ use Bio::Phylo::IO 'parse_tree';
 use Bio::Phylo::PhyLoTA::Config;
 use Bio::Phylo::PhyLoTA::Service;
 use base 'Bio::Phylo::PhyLoTA::Service';
+use Bio::Phylo::PhyLoTA::Domain::MarkersAndTaxa;
+use Storable 'dclone';
 
 my $config = Bio::Phylo::PhyLoTA::Config->new;
 my $log = Bio::Phylo::PhyLoTA::Service->logger;	
@@ -16,11 +18,54 @@ sub write_tree {
 
 }
 
-# it appears (obviously) that midpoint rooting is going to fail in some
-# cases. How to automate this? One possibility is that the common tree might
-# be of use.
-sub reroot_tree {
-	
+# re-roots a given backbone tree such that the number of monophyletic genera is maximized 
+sub reroot_tree {       
+	my ($self, $tree, @records) = @_;
+        if (! @records) {
+                die "Need both, tree and species table!";
+        }
+        $log->info("rerooting tree");
+        my $nw = $tree->to_newick;
+
+        # iterate over all nodes, re-root the tree at the branch below each
+        # node and check how many clades we will get. Take the tree that will 
+        # have the most clades.
+        my $max_clades = scalar ( $self->_make_clade_species_sets( $tree, @records ) );
+        my $num_internals = scalar @{ $tree->get_internals };
+        my $result = $tree;
+
+        foreach my $i ( 0..$num_internals-1 ) {
+                # Get a fresh curr_tree, because node order could be messed up
+                # by re-rooting and not all internal branches would be considered.
+                # Reading from newick is a workaround since $tree->clone somehow takes forever
+                my $curr_tree = parse_tree(
+                        '-format'     => 'newick',
+                        '-string'     => $nw,
+                    );                                
+                my $node = @{ $curr_tree->get_internals }[$i];                
+                $curr_tree->reroot($node);
+                my @set = $self->_make_clade_species_sets( $curr_tree, @records );
+                if ( scalar (@set) > $max_clades ){
+                        $max_clades = scalar (@set);
+                        $result = $curr_tree;
+                }
+        }
+        $log->info("rerooted tree has $max_clades monophyletic clades");
+        return $result;
+}
+
+# Changes NCBI taxon identifiers in a given tree to taxon names
+sub remap {
+        my ($self, $tree) = @_;
+        $tree->visit(sub{
+                my $n = shift;
+                if ( $n->is_terminal ) {
+                        my $id = $n->get_name;
+                        $n->set_name( $self->find_node($id)->taxon_name );
+                }
+                     });
+        return $tree;
+        
 }
 
 # -infile, (optional: -burnin)
@@ -45,8 +90,8 @@ sub consense_trees {
 	# execute command
 	my $tmpl = '%s -burnin %i %s %s 2> /dev/null';
 	my $command = sprintf $tmpl, $config->TREEANNOTATOR_BIN, $babs, $infile, $outfile;
-	system($command) and die "Error building consensus: $?";
-	# return the resulting tree
+        system($command) and die "Error building consensus: $?";
+        # return the resulting tree
 	return parse_tree(
 	        '-format' => 'nexus',
 		'-file'   => $outfile,
@@ -66,7 +111,7 @@ sub graft_tree {
 	for my $name ( @names ) {
 		if ( my $e = $backbone->get_by_name($name) ) {
 			$log->info("found $name in backbone: ".$e->get_name);
-			push @exemplars, $e;
+                        push @exemplars, $e;
 		}
 		else {
 			$log->debug("$name is not in the backbone tree");
@@ -85,6 +130,7 @@ sub graft_tree {
 	# calculate the respective depths, scale the clade by the ratios of depths
 	my $cmrca_depth = $cmrca->calc_max_path_to_tips;
 	my $bmrca_depth = $bmrca->calc_max_path_to_tips;
+        my $str = $clade->to_newick;
 	$clade->visit(sub{
 		my $node = shift;
 		my $bl = $node->get_branch_length || 0; # zero for root
@@ -191,7 +237,6 @@ sub make_phylip_from_matrix {
 # reads the supermatrix (phylip format) and returns the tip names from it
 sub read_tipnames {
 	my ($self, $supermatrix) = @_;
-        my $logger = $self->logger;	
 	my $ntax = 0;
 	my $line = 0;
 	my @result;	
@@ -204,16 +249,126 @@ sub read_tipnames {
                 }
 		if ( not $ntax ) {
 			$ntax = $word;                        
-			$logger->debug("$supermatrix has $ntax taxa");
+			$log->debug("$supermatrix has $ntax taxa");
                         next LINE;
                 }
 		if ( $word ) {
                         push @result, $word;
                 };
-		$logger->debug("adding taxon $word");
+		$log->debug("adding taxon $word");
 		last LINE if ++$line == $ntax;
 	}
 	return @result;
+}
+
+
+
+## as
+sub _make_clade_species_sets {
+        my ($self, $tree, @records) = @_;
+        if (! @records) {
+                die "Need both, tree and species table!";
+        }                
+        my $mt = 'Bio::Phylo::PhyLoTA::Domain::MarkersAndTaxa';
+
+        # traverse tree, retrieve monophyletic genera, which we will compare to all
+        $log->info("going to identify putatively monophyletic genera");
+        my ( %monophyletic, %all );
+        $tree->visit_depth_first(
+                '-post' => sub {
+                        my $node = shift;
+                        
+                        # for the tips, we keep a running tally of all seen genera. if a genus 
+                        # occurs on more than one tip we need to know if the genus is monophyletic
+                        if ( $node->is_terminal ) {
+                                my $id = $node->get_name;
+                                my ($genus) = map { $_->{'genus'} } grep { $_->{'species'} == $id } @records;
+                                $node->set_generic( 'species' => [ $id ], 'genera' => { $genus => 1 } );
+                                
+                                # in the end, this will be either 1 (for monotypic genera), or 2 (for
+                                # exemplar genera).
+                                $all{$genus}++;
+                        }
+                        else {
+                                
+                                # here we simply carry over the species and genera from the
+                                # children to the focal node. in the second pass we will use
+                                # this to find the shallowest node that subtends all species
+                                # of a paraphyletic genus
+                                my ( @s, %g );						
+                                for my $child ( @{ $node->get_children } ) {
+                                        push @s, @{ $child->get_generic('species') };
+                                        my %genus = %{ $child->get_generic('genera') };
+                                        $g{$_} += $genus{$_} for keys %genus;
+                                }
+                                $node->set_generic( 'species' => \@s, 'genera' => \%g );
+                                
+                                # the node subtends two species that all belong to the same genus. 
+                                # hence, the node is monophyletic.
+                                if ( scalar(@s) >= 2 and scalar keys %g == 1 ) {
+                                        $monophyletic{$_} += $g{$_} for keys %g;
+                                }
+                        }
+                }
+            );        
+        
+        # all the genera that aren't putatively monophyletic but that do have two
+        # members are therefore paraphyletic. in the second pass we lump all the
+        # putatively monophyletic genera that nest inside paraphyletic ones within
+        # the mixed set of mono/para.
+        $log->info("going to reconstruct paraphyly");
+        my %paraphyletic = map { $_ => 1 } grep { !$monophyletic{$_} && $all{$_} == 2 } keys %all;
+        my @genera;
+        $tree->visit_depth_first(
+                '-post' => sub {
+                        my $node = shift;
+                        if ( $node->is_internal ) {
+                                my %g = %{ $node->get_generic('genera') };
+                                
+                                # the node is a paraphyletic mrca if it is the shallowest
+                                # node where a paraphyletic genus occurs and where it
+                                # subtends the two exemplars from that genus
+                                my $is_para;
+                                for my $genus ( keys %g ) {
+                                        if ( $paraphyletic{$genus} and $g{$genus} == 2 ) {
+                                                $is_para++;
+                                        }
+                                }
+                                
+                                # if the node is paraphyletic we store ALL its subtended
+                                # genera, removing them from the set of putative monophyletic
+                                # genera as well as from the paraphyletic ones. we remove
+                                # from the former set so that after this traversal we don't
+                                # put monophyletic genera that nest inside paraphyletic ones
+                                # in a separate set, and we remove the latter set so that
+                                # deeper nodes that also subtend the paraphyletic genera
+                                # don't trigger processing.
+                                if ( $is_para ) {
+                                        my @g = keys %g;
+                                        push @genera, \@g;
+                                        delete @paraphyletic{@g};
+                                        delete @monophyletic{@g};
+                                }	
+                        }	
+                }
+            );
+        # all remaining ones become their own set
+        push @genera, [ $_ ] for keys %monophyletic;
+        # now resolve the nesting of paraphyletic genera
+        my %index;
+        for my $i ( 0 .. $#genera ) {
+                $index{$_} = $i for @{ $genera[$i] };                
+        }
+        
+        # make species sets
+        my @set;
+        for my $i ( keys %{ { map { $_ => 1 } values %index } } ) {
+                my @g = @{ $genera[$i] };
+                my @s = map { $mt->get_species_for_taxon( 'genus' => $_, @records ) } @g;
+                push @set, \@s if scalar(@s) > 2;
+        }
+        
+        return @set;        
 }
 
 
--- t/treeservice.t
+++ t/treeservice.t
@@ -4,6 +4,7 @@ use warnings;
 use FindBin '$Bin';
 use Test::More 'no_plan';
 use Bio::Phylo::IO 'parse_tree';
+use Bio::Phylo::PhyLoTA::Domain::MarkersAndTaxa 'parse_taxa_file';
 
 my $primates = "$Bin/../examples/primates";
 
@@ -13,15 +14,34 @@ BEGIN { use_ok('Bio::Phylo::PhyLoTA::Service::TreeService'); }
 # create a new instance
 my $ts = new_ok('Bio::Phylo::PhyLoTA::Service::TreeService');
 
+my $tf = "$primates/megatree.dnd";
+my $tree = parse_tree(
+	'-file'   => $tf,
+	'-format' => 'newick',
+	'-as_project' => 1,
+    );
+
+my $taxa = "$primates/species.tsv";
+
+# parse taxon mapping
+my $mt = 'Bio::Phylo::PhyLoTA::Domain::MarkersAndTaxa';
+my @records = $mt->parse_taxa_file($taxa);
+
+my $rerooted = $ts->reroot_tree($tree, @records);
+
+isa_ok ($rerooted, 'Bio::Tree::TreeI');
+
+# rerooted tree should be different
+ok(! ($tree->to_newick eq $rerooted->to_newick));
+
 # parse phylip files for tree tips
 my $phylip_small = "$Bin/example.phy";
 my @names_small = $ts->read_tipnames($phylip_small);
 is(scalar @names_small, 5, "read_tipnames from phylip file");
 
-#my $phylip_large = "$Bin/example2.phy";
-#my @names_large = $ts->read_tipnames($phylip_large);
-#print "Names large :".$names_large[0]."\n";
-#is($names_large[0], "Orthogeomys_heterodus", "read_tipnames from phylip file");
+my $phylip_large = "$Bin/example2.phy";
+my @names_large = $ts->read_tipnames($phylip_large);
+is($names_large[0], "Orthogeomys_heterodus", "read_tipnames from phylip file");
 
 # build a consensus tree
 my $treefile  = "$primates/clade3/clade3.nex";
@@ -37,6 +57,3 @@ my $bbtree = parse_tree(
 );
 my $grafted = $ts->graft_tree( $bbtree, $consensus );
 isa_ok($grafted,'Bio::Tree::TreeI');
-
-
-
