d07d896a75ae0d4b7b08dcf1afbe14e06db0fda3
hettling <hannes.hettling@gmail.com>
2014-08-03 17:09:09 +0200

    added command-line option to provide a root taxon name instead of a whole names file

--- script/supersmart/parallel_write_taxa_table.pl
+++ script/supersmart/parallel_write_taxa_table.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use Getopt::Long;
 use Bio::Phylo::Util::Logger ':levels';
+use Bio::Phylo::PhyLoTA::Config;
 use Bio::Phylo::PhyLoTA::Service::MarkersAndTaxaSelector;
 use Bio::Phylo::PhyLoTA::Service::ParallelService 'mpi'; # can be either 'pthreads' or 'mpi';
 
@@ -12,11 +13,11 @@ parallel_write_taxa_table.pl - reconciles taxon labels with a taxonomy
 
 =head1 SYNOPSYS
 
- $ mpi_write_taxa_table.pl --infile=<file> > <outfile>
+ $ mpi_write_taxa_table.pl --infile=<file> --root_taxon<taxon_name>  > <outfile>
 
 =head1 DESCRIPTION
 
-Given a file with putative taxon names, attempts to link these to the pipeline's
+Given a file with putative taxon names, or optionally a root taxon name, attempts to link these to the pipeline's
 underlying taxonomy. Writes the results as a tab-separated file to STDOUT.
 
 =cut
@@ -24,9 +25,10 @@ underlying taxonomy. Writes the results as a tab-separated file to STDOUT.
 # process command line arguments
 my $verbosity = WARN;
 my @levels = qw[species genus family order class phylum kingdom];
-my ( $infile );
+my ( $infile, $root_taxon );
 GetOptions(
-	'infile=s' => \$infile,
+	'infile=s'   => \$infile,
+	'root_taxon=s' => \$root_taxon,
 	'verbose+' => \$verbosity,
 	'level=s'  => \@levels,
 );
@@ -36,30 +38,41 @@ my $log = Bio::Phylo::Util::Logger->new(
 	'-level' => $verbosity,
 	'-class' => [qw(
 		main 
+		
 	)]
     );
 
+my $config = Bio::Phylo::PhyLoTA::Config->new;
 my $mts = Bio::Phylo::PhyLoTA::Service::MarkersAndTaxaSelector->new;
 
-# read names from file or STDIN, clean line breaks
+# if 'infile' argument is given, read names from file or STDIN, clean line breaks
 my @names;
-if ( $infile eq '-' ) {
-	@names = <STDIN>;
-	chomp(@names);
-	$log->info("read species names from STDIN");
-}
-else {
-	open my $fh, '<', $infile or die $!;
-	@names = <$fh>;
-	chomp(@names);
-	$log->info("read ".scalar(@names)." species names from $infile");
+if ( $infile ){
+	if ( $infile eq '-' ) {
+		@names = <STDIN>;
+		chomp(@names);
+		$log->info("read species names from STDIN");
+	}
+	else {
+		open my $fh, '<', $infile or die $!;
+		@names = <$fh>;
+		chomp(@names);
+		$log->info("read ".scalar(@names)." species names from $infile");
+	}
 }
 
-@names = $mts->expand_taxa( @names );
+# if a root taxon is given, add it to the taxon names and set expansion to "Species" if not set otherwise
+if ( $root_taxon ) {
+	push @names, $root_taxon;
+}
+
+# expand possible root taxa to taxonomic level set in ROOT_TAXA_EXPANSION
+my $expand_to = $config->ROOT_TAXA_EXPANSION || "Species";
+	
+@names = $mts->expand_taxa( \@names , $expand_to);
 
 # print table header to stdout in sequential mode
 sequential { print join ("\t", 'name', @levels), "\n"; };
-
 
 # this will take some time to do the taxonomic name resolution in the
 # database and with webservices. The below code runs in parallel
