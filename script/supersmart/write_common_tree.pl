071f41e95359e2b55d8ca68f45f9fbd4d2d6ebd0
hettling <hannes.hettling@gmail.com>
2014-10-02 18:08:04 +0200

    this fixes the situation with empty guid's for nodes, at least for the case of the Zygopetalinae. Further test if it will work with the angiosperms is needed.

--- script/supersmart/write_common_tree.pl
+++ script/supersmart/write_common_tree.pl
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 use Getopt::Long;
-use Bio::Phylo::IO 'unparse';
+use Bio::Phylo::IO qw(parse_tree unparse);
 use Bio::Phylo::Util::Logger ':levels';
 use Bio::Phylo::PhyLoTA::Service::MarkersAndTaxaSelector;
 use Bio::Phylo::PhyLoTA::Domain::MarkersAndTaxa;
@@ -25,18 +25,14 @@ taxonomic levels up to the root), and with the taxon IDs as terminal labels.
 =cut
 
 # process command line arguments
-my $template   = '${get_guid}';  # by default, the node label is the NCBI taxon ID
-my @properties = qw(get_guid); # additional column values to use in template
 my $infile     = '-'; # read from STDIN by default
 my $outformat  = 'newick'; # write newick by default
 my $nodelabels = 1; # write internal node labels by default
 my $verbosity  = INFO; # low verbosity
 GetOptions(
-	'template=s'  => \$template,
 	'infile=s'    => \$infile,
 	'outformat=s' => \$outformat,
 	'verbose+'    => \$verbosity,
-	'property=s'  => \@properties,
 	'nodelabels'  => $nodelabels,
 );
 
@@ -45,7 +41,7 @@ my $mts = Bio::Phylo::PhyLoTA::Service::MarkersAndTaxaSelector->new;
 my $mt =  Bio::Phylo::PhyLoTA::Domain::MarkersAndTaxa->new;
 
 my $log = Bio::Phylo::Util::Logger->new(
-	'-class' => [ 'main' ],
+	'-class' => [ 'main', 'Bio::Phylo::PhyLoTA::Service::MarkersAndTaxaSelector' ],
 	'-level' => $verbosity
 );
 
@@ -59,21 +55,13 @@ my @nodes = $mts->get_nodes_for_table( @taxatable );
 my $tree = $mts->get_tree_for_nodes(@nodes);
 $log->debug("done computing common tree");
 
+my $newick = $tree->to_newick;
+
 # create node labels
 $tree->visit(sub{
 	my $node = shift;
 	if ( $nodelabels or $node->is_terminal ) {
-		my $label;		
-		my $statement = "my (" . join( ',', map ("\$$_", @properties) ). ");\n";
-		for my $property ( @properties ) {
-			$statement .= "\$$property = q[" . $node->$property . "];\n";
-			$log->debug($statement);
-		}		
-		$statement .= "\$label = $template";
-		$log->debug($statement);
-		eval $statement;
-		die $@ if $@;
-		$log->debug($label);
+		my $label = $node->get_guid;		
 		$node->set_name( $label );
 	}
 });
