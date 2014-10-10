ab4227961d16200b46e8c4e3e5dfe173e7ab6823
hettling <hannes.hettling@gmail.com>
2014-10-10 17:47:02 +0200

    Now making help screen with parsed description from POD documentation for each subcommand class.

--- lib/Bio/Apps/Supersmart.pm
+++ lib/Bio/Apps/Supersmart.pm
@@ -2,18 +2,28 @@ package Bio::Apps::Supersmart;
 our $VERSION = 0.1;
 use App::Cmd::Setup -app;
 
-
 =head1 NAME
 
-Bio::Apps::Supersmart - 
+Bio::Apps::Supersmart app class
 
 =head1 DESCRIPTION
+
+This class is invoked when using the command C<smrt>.
 
 =cut
 
 1;
 
 package Bio::Apps::GlobalCmd;
+
+=head1 NAME
+
+Bio::Apps::GlobalCmd Superclass for C<smrt> subcommands
+
+=head1 DESCRIPTION
+
+This class provides global options (e.g. verbosity, working directory) that are
+inherited by all subcommand classes.
 
 =head1 METHODS
 
@@ -30,10 +40,68 @@ subcommand specific options.
 sub opt_spec {
 	my ($class, $app) = @_;		
 	return (
-		["verbose|v+", "increase verbosity level", {}],
-		["workdir|w=s", "rank to which root taxa are expanded", { default => ".", arg => "dir"}],	
+		[ "help|h", "display help screen", {} ],
+		[ "verbose|v+", "increase verbosity level", {} ],
+		[ "workdir|w=s", "directory in which results and intermediate files are stored", { default => ".", arg => "dir"} ],	
 		$class->options($app),
 	);	
+}
+
+=item description
+
+Returns the description for a specific subcommand. This method is invoked by 
+the child classes when the global option 'help' is used, e.g. in 
+$ smrt help subcommand
+The actual description of the subcommand is parsed from it's DESCRIPTION field
+in the POD documentation.
+
+=cut
+
+sub description {
+	my ($class, $opt, $args) = @_;
+
+	# classname to filename
+  	my $pm_file = $class;
+  	$pm_file =~ s/::/\//g; 
+  	$pm_file =~ s/=.*//g;   	
+  	$pm_file .= '.pm';
+  	$pm_file = $INC{$pm_file} or return "(unknown)";
+	
+  	# if the pm file exists, open it and parse it
+  	open my $fh, "<", $pm_file or return "(unknown)";
+	undef $/;	
+	my $content = <$fh>;
+	my ($desc) = 
+         ($content =~ m/.*?DESCRIPTION\n(.*?)\n=.*/s);	
+	close $fh;
+	$content =~ s/^\s+//;	
+	return $desc;
+}
+
+=item validate_args
+
+overrides the default method mainly for one reason: There are two ways of 
+displaying the help screen: One is '$ smrt help command', the other is
+'$ smrt command -h'. Here we make the latter to behave exactly as
+the former.
+
+=cut
+
+sub validate_args {
+	my ($class, $opt, $args) = @_;		
+	
+	if ($opt->help){
+			
+		# This is a small hack to make the help screen called with the 
+		#  option (as here, e.g. "$ smrt command -h" ) look the same
+		#  as when we call "$ smrt help command"  				
+		my ($cmd) = $class->app->prepare_command("help"); 
+		my @name = ($class->command_names);
+		$cmd->execute($opt, \@name);
+		exit;
+	} else {
+		$class->validate($opt, $args);
+	}
 }
 
 =item usage_desc
@@ -66,5 +134,7 @@ sub usage_desc {
 	}
 	return $usage;	
 }
+
+=back
 
 1;
