eb7a7a45a208db432e8fa323e3fac1139a6a6f79
hettling <hannes.hettling@gmail.com>
2014-10-23 11:22:37 +0200

    bbdescompose can now write a report table with all included accessions for all species

--- lib/Bio/SUPERSMART/App/smrt/Command/BBdecompose.pm
+++ lib/Bio/SUPERSMART/App/smrt/Command/BBdecompose.pm
@@ -3,6 +3,8 @@ package Bio::SUPERSMART::App::smrt::Command::BBdecompose;
 use strict;
 use warnings;
 
+use List::MoreUtils 'uniq';
+	
 use Bio::Phylo::IO 'parse_tree';
 use Bio::Phylo::PhyLoTA::Config;
 use Bio::Phylo::PhyLoTA::Domain::MarkersAndTaxa;
@@ -15,15 +17,17 @@ use base 'Bio::SUPERSMART::App::smrt::SubCommand';
 use Bio::SUPERSMART::App::smrt qw(-command);
 
 
-
 sub options {
 	my ($self, $opt, $args) = @_;		
+	my $outfile_default="summary.tsv";
 	return (
 		["backbone|b=s", "backbone tree as produced by 'smrt bbinfer'", { arg => "file", mandatory => 1}],
 		["commontree|c=s", "classification tree as produced by 'smrt classify'", { arg => "file", mandatory => 1}],
 		["alnfile|a=s", "list of file locations of merged alignments  as produced by 'smrt orthologize'", { arg => "file", mandatory => 1}],	
 		["taxafile|t=s", "tsv (tab-seperated value) taxa file as produced by 'smrt taxize'", { arg => "file", mandatory => 1}],
-		[ "add_outgroups|o", "automatically add outgroup for each clade", { default=> 1} ],
+		["add_outgroups|g", "automatically add outgroup for each clade", { default=> 1} ],
+		["summary_table|s=s", ", writes to 'markers.tsv' by default", ],
+		["outfile|o=s", "name of the output file (summary table with included accessions)", { default=> $outfile_default, arg => "file"}],
 	);	
 }
 
@@ -33,7 +37,6 @@ sub validate {
 	#  If alignment or taxa file is absent or empty, abort  
 	my @files = ( $opt->alnfile, $opt->backbone, $opt->taxafile, $opt->commontree );
 	foreach my $file ( @files ){
-		print $file . "\n";
 		$self->usage_error("need alignment and taxa files and classification and backbone tree file") if not $file;
 		$self->usage_error("file $file does not exist") unless (-e $file);
 		$self->usage_error("file $file is empty") unless (-s $file);			
@@ -50,7 +53,7 @@ sub run{
 	my $common = $opt->commontree;
 	my $add_outgroup = $opt->add_outgroups;
 	(my $workdir = $opt->workdir) =~ s/\/$//g;
-	#my $outfile= $self->outfile;	
+	my $outfile= $self->outfile;	
 
 	# instantiate helper objects
 	my $mt = 'Bio::Phylo::PhyLoTA::Domain::MarkersAndTaxa';
@@ -71,7 +74,6 @@ sub run{
 		'-file'       => $common,
 		'-as_project' => 1,
 	);
-			
 			
 	# parse taxon mapping
 	$logger->info("going to read taxa mapping $taxafile");
@@ -97,18 +99,23 @@ sub run{
 		foreach (@set){
 			my @og = $mts->get_outgroup_taxa( $commontree, $_ );
 			push @{$_}, @og;
-			$logger->info("Adding outgroup species " . join (' ,', @og) . " to clade #" . ++$counter);
+			$logger->info("Adding outgroup species " . join (', ', @og) . " to clade #" . ++$counter);
 		}	
 	}
 	
 	# write suitable alignments to their respective clade folders
-	pmap {
+	#  and return a table with markers for all species
+	my @table = pmap {
 	    my $aln = $_;
-		$logger->info("assessing alignment file $aln");
+		$logger->info("checking whether alignment $aln can be included");
 		my %fasta = $mt->parse_fasta_file($aln);
 		my $dist  = $mt->calc_mean_distance(%fasta);
 		my $nchar = $mt->get_nchar(%fasta);
 		my $mdens = $config->CLADE_MIN_DENSITY;
+		
+		# return value: all included sequences per species
+		my %ret;
+		
 		if ( $dist <= $config->CLADE_MAX_DISTANCE ) {
 		
 			# assess for each set whether we have enough density
@@ -134,23 +141,80 @@ sub run{
 							for my $j ( 0 .. $#{ $seq{$s} } ) {
 								my $seq = $seq{$s}->[$j];
 								print $fh '>', $seq, "\n", $fasta{$seq}, "\n";
+								$ret{$s} = $seq;		
 							}
 						}
 					}
+				}
+				else {
+					my $dens = sprintf ( "%.2f", $distinct/scalar(@species));
+					$logger->info("$aln is not informative or dense enough: density " 
+					. $dens . " > $mdens, number of distinct taxa: $distinct");
 				}
 			}
 		}
 		else {
 			$logger->info("$aln is too divergent: $dist > ".$config->CLADE_MAX_DISTANCE);
 		}
-		$logger->info("done assessing alignment file $aln");
+		return \%ret;
 	} @alignments;
 	
-	$logger->info("DONE, results written to $workdir");
+	my @all_species = uniq map {@$_} @set;
+	
+	_write_marker_summary( $outfile, $mts, \@table, \@all_species );
+
+	$logger->info("DONE, results written into working directory $workdir");
 
 	return 1;
 }
 
+
+# writes a table containing all species as rows and all chosen markers 
+#  as columns, reports the genbank accession of a specific marker for a species
+sub _write_marker_summary {
+	my ( $file, $mts, $tab, $specs ) = @_;
+	my @table = @$tab;
+	my @all_species = @$specs;
+	
+	# remove empty columns (for markers that are never included)
+	@table = grep {keys %{$_}} @table;
+	
+	my @seed_gis = map { (values(%{$_}))[0] =~ /seed_gi\|([0-9]+)\|/ } @table;
+	
+	open my $outfh, '>', $file or die $!;
+	
+	# print table header
+	print $outfh "taxon\t";
+	print $outfh "marker" . $_ ."\t" for 1..$#seed_gis+1;
+	print $outfh "\n";
+	foreach my $species ( @all_species ) {
+		my $name = $mts->find_node($species)->taxon_name;
+		print $outfh $name . "\t";
+		foreach my $column ( @table ) {
+			my %h = %{$column};
+			if (  my $seq = $h{$species} ) {
+				# parse the GI from fasta descripion
+				my ($gi) = $seq =~ /gi\|([0-9]+)\|/;
+				my $seqobj = $mts->find_seq($gi);
+				print $outfh $seqobj->acc . "\t";
+			}
+			else {
+				print $outfh "\t";
+			}
+		}
+		print $outfh "\n";
+	}
+	print $outfh "\n";
+	
+	# print information about markers at the bottom of the table
+	foreach my $i ( 1..$#seed_gis+1 ) {
+		my $seqobj = $mts->find_seq( $seed_gis[$i-1] );
+		print $outfh "# marker$i cluster seed: " . $seqobj->acc . ", description: " . $seqobj->def . "\n"; 
+	}
+	close $outfh;
+}
+
+# makes file handle for alignment file to be written in clade directory
 sub _make_handle {
 	my ( $i, $aln, $workdir ) = @_;
 	if ( not -d "$workdir/clade$i" ) {
