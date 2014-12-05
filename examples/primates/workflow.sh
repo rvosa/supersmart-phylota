a9f8402c27c26a16c39bc50997d84b62f389665b
hettling <hannes.hettling@gmail.com>
2014-12-05 13:29:13 +0100

    corrected errors

--- examples/primates/workflow.sh
+++ examples/primates/workflow.sh
@@ -2,11 +2,11 @@ smrt taxize -i names.txt
 smrt classify -i species.tsv
 smrt align -i species.tsv
 smrt orthologize -i aligned.txt
-smrt bbmerge -a merged.txt-t species.tsv
-smrt bbinfer -s supermatrix.phy-t classification-tree.dnd-i examl
-smrt bbreroot -b backbone.dnd-t species.tsv
-smrt bbcalibrate -t backbone-rerooted.dnd-s supermatrix.phy-f fossils.tsv-o backbone-calibrated.dnd
-smrt bbdecompose -b backbone-calibrated.dnd-c classification-tree.dnd-a merged.txt-t species.tsv
+smrt bbmerge -a merged.txt -t species.tsv
+smrt bbinfer -s supermatrix.phy -t classification-tree.dnd -i examl
+smrt bbreroot -b backbone.dnd -t species.tsv
+smrt bbcalibrate -t backbone-rerooted.dnd -s supermatrix.phy -f fossils.tsv -o backbone-calibrated.dnd
+smrt bbdecompose -b backbone-calibrated.dnd -c classification-tree.dnd -a merged.txt -t species.tsv
 smrt clademerge  
 smrt cladeinfer 
 smrt cladegraft -b backbone-calibrated.dnd
