7c0b899da10433cdd9992d1ecae7d36ef9e234dc
hettling <hannes.hettling@gmail.com>
2014-12-05 13:25:23 +0100

    removing relative paths as they are not needed anymore

--- examples/primates/workflow.sh
+++ examples/primates/workflow.sh
@@ -1,23 +1,12 @@
-app/smrt taxize -w examples/primates/ -i examples/primates/names.txt
-
-app/smrt classify -w examples/primates/ -i examples/primates/species.tsv
-
-app/smrt align -w examples/primates/ -i examples/primates/species.tsv
-
-app/smrt orthologize -w examples/primates/ -i examples/primates/aligned.txt
-
-app/smrt bbmerge -w examples/primates/ -a examples/primates/merged.txt -t examples/primates/species.tsv
-
-app/smrt bbinfer -w examples/primates/ -s examples/primates/supermatrix.phy -t examples/primates/classification-tree.dnd -i examl
-
-app/smrt bbreroot -w examples/primates/ -b examples/primates/backbone.dnd -t examples/primates/species.tsv
-
-app/smrt bbcalibrate -t examples/primates/backbone-rerooted.dnd -s examples/primates/supermatrix.phy -f examples/primates/fossils.tsv -o examples/primates/backbone-calibrated.dnd
-
-app/smrt bbdecompose -w examples/primates/ -b examples/primates/backbone-calibrated.dnd -c examples/primates/classification-tree.dnd -a examples/primates/merged.txt -t examples/primates/species.tsv
-
-app/smrt clademerge -w examples/primates/ 
-
-app/smrt cladeinfer -w examples/primates
-
-app/smrt cladegraft -w examples/primates/ -b examples/primates/backbone-calibrated.dnd 
+smrt taxize -i names.txt
+smrt classify -i species.tsv
+smrt align -i species.tsv
+smrt orthologize -i aligned.txt
+smrt bbmerge -a merged.txt-t species.tsv
+smrt bbinfer -s supermatrix.phy-t classification-tree.dnd-i examl
+smrt bbreroot -b backbone.dnd-t species.tsv
+smrt bbcalibrate -t backbone-rerooted.dnd-s supermatrix.phy-f fossils.tsv-o backbone-calibrated.dnd
+smrt bbdecompose -b backbone-calibrated.dnd-c classification-tree.dnd-a merged.txt-t species.tsv
+smrt clademerge  
+smrt cladeinfer 
+smrt cladegraft -b backbone-calibrated.dnd
