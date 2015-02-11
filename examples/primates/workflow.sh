64642576512329fe5aa1cebfeed095e10b66a40c
hettling <hannes.hettling@gmail.com>
2015-02-11 17:29:48 +0100

    now bbdecompose takes the correct alignment file

--- examples/primates/workflow.sh
+++ examples/primates/workflow.sh
@@ -6,7 +6,7 @@ smrt bbmerge -a merged.txt -t species.tsv
 smrt bbinfer -s supermatrix.phy -t classification-tree.dnd -i examl
 smrt bbreroot -b backbone.dnd -t species.tsv
 smrt bbcalibrate -t backbone-rerooted.dnd -s supermatrix.phy -f fossils.tsv -o backbone-calibrated.dnd
-smrt bbdecompose -b backbone-calibrated.dnd -c classification-tree.dnd -a merged.txt -t species.tsv
+smrt bbdecompose -b backbone-calibrated.dnd -c classification-tree.dnd -a aligned.txt -t species.tsv
 smrt clademerge  
 smrt cladeinfer 
 smrt cladegraft -b backbone-calibrated.dnd
