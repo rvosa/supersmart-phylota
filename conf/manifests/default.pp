e0bb31eb293b78d9218da6725102de9dfa67b4f7
hettling <hannes.hettling@gmail.com>
2015-04-21 17:23:29 +0200

    making an attempt to run exabayes on the travis vm

--- conf/manifests/default.pp
+++ conf/manifests/default.pp
@@ -113,18 +113,18 @@ file {
     "exabayes_link":
         path    => "${tools_bin_dir}/exabayes",
         ensure  => link,
-        target  => "${tools_dir}/exabayes-1.4.1/bin/bin/exabayes",
-        require => Exec["download_exabayes"];
+        target  => "${tools_dir}/exabayes-1.4.1/exabayes",
+        require => Exec["compile_exabayes"];
     "parse_exabayes_link":
         path  => "${tools_bin_dir}/parse-exabayes",
         ensure  => link,
-        target  => "${tools_dir}/exabayes-1.4.1/bin/bin/parser",
-        require => Exec["download_exabayes"];
+        target  => "${tools_dir}/exabayes-1.4.1/parser",
+        require => Exec["compile_exabayes"];
     "exabayes_consense_link":
         path  => "${tools_bin_dir}/consense-exabayes",
         ensure  => link,
-        target  => "${tools_dir}/exabayes-1.4.1/bin/bin/consense",
-        require => Exec["download_exabayes"];
+        target  => "${tools_dir}/exabayes-1.4.1/consense",
+        require => Exec["compile_exabayes"];
     "raxml_link":
         path    => "${tools_bin_dir}/raxml",
         ensure  => link,
@@ -307,6 +307,12 @@ exec {
         cwd     => $tools_dir,
         creates => "${tools_dir}/exabayes-1.4.1/",
         require => Exec[ "download_exabayes" ];
+    "compile_exabayes":
+      	command => "sh configure --enable-mpi && make",
+      	cwd     => "${tools_dir}/exabayes-1.4.1",
+      	creates => "${tools_dir}/exabayes",
+      	timeout => 0,
+      	require => Exec[ "unzip_exabayes" ];
 
     # install supersmart
     "clone_supersmart":
