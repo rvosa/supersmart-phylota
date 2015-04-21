0f201141097be2c8aeba11b2dcece11f9ea39107
Rutger Vos <rutgeraldo@gmail.com>
2015-04-21 21:51:57 +0200

    viz using onezoom and jsphylosvg now available (#38)

--- lib/Bio/SUPERSMART/App/smrtutils/Command/Plot.pm
+++ lib/Bio/SUPERSMART/App/smrtutils/Command/Plot.pm
@@ -1,12 +1,9 @@
 package Bio::SUPERSMART::App::smrtutils::Command::Plot;
-
 use strict;
 use warnings;
-
-use Bio::Phylo::IO qw(parse);
-
 use Template;
-
+use Bio::Phylo::IO qw(parse_tree);
+use Bio::SUPERSMART::App::SubCommand;
 use base 'Bio::SUPERSMART::App::SubCommand';
 use Bio::SUPERSMART::App::smrtutils qw(-command);
 
@@ -32,26 +29,33 @@ Creates a file with a graphical representation of a tree. Currently supported fo
 
 =cut
 
-my %formats = ('onezoom' => 'htm');
+my %formats = ( 
+	'onezoom' => 'html', 
+);
 
 sub options {
     
 	my ($self, $opt, $args) = @_;
-	my $format_default = 'onezoom';
+	my $format_default  = 'onezoom';
 	my $outfile_default = "$format_default.$formats{$format_default}";
+	my $width_default   = 800;
+	my $height_default  = 600;
+	my $style_default   = 'rectangular';
 	return (
-		['tree|t=s', 'file name of input tree (newick format)', { arg => 'file', mandatory => 1}],
-		['outfile|o=s', "name of the output file, defaults to '$outfile_default'", {default => $outfile_default, arg => 'file'}],	
-		['format|f=s', "output format, currently supported: OneZoom. Default: $format_default", {default=>$format_default} ],
-		['show|s', 'show generated figure in default tool, note that perl package Browser::Open must be installed for this option to work with OneZoom', {}],
-	    );	
+		['tree|t=s', 'file name of input tree (newick format)', { arg => 'file', mandatory => 1 } ],
+		['outfile|o=s', "name of the output file, defaults to '$outfile_default'", { default => $outfile_default, arg => 'file' } ],	
+		['format|f=s', "output format, currently supported: OneZoom, jsPhyloSVG. Default: $format_default", { default => $format_default } ],
+		['width|w=i',"visualization width. Default: $width_default", { default => $width_default } ],
+		['height|h=i',"visualization height. Default: $height_default", { default => $height_default } ],
+		['style|s=s',"visualization style (rectangular or circular). Default: $style_default", { default => $style_default } ],
+	);	
 }
 
 sub validate {
 	my ($self, $opt, $args) = @_;		
 	
-	# We only have to check the 'infile' argument. 
-	#  If the infile is absent or empty, abort  
+	# we only have to check the 'infile' argument. 
+	# if the infile is absent or empty, abort  
 	my $file = $opt->tree;
 	$self->usage_error('no tree argument given') if not $file;
 	$self->usage_error('file $file does not exist') unless (-e $file);
@@ -59,43 +63,30 @@ sub validate {
 }
 
 sub run {
-	my ($self, $opt, $args) = @_;    
-	my $treefile = $opt->tree;
+	my ($self, $opt, $args) = @_;
 	my $outfile = $opt->outfile;
-	my $logger = $self->logger;
+	my $logger  = $self->logger;
 	
 	# read tree
-	my $treestr = parse(
-		'-file'   => $treefile,
+	my $tree = parse_tree(
+		'-file'   => $opt->tree,
 		'-format' => 'newick',
-	    )->first->to_newick;
+	);
 	
-	# get onezoom template file and add newick string
-	my $tool = $opt->format;
-	my $file = $ENV{SUPERSMART_HOME} . '/data/VISUALIZATION/' . $tool . '.tmpl';
-	
-	
-	my %args = (tree=>"$treestr");
-	my $tt = Template->new(ABSOLUTE=>1);
-	my $oz = $tt->process($file, \%args, $outfile) or die $tt->error;
-	
-	if ( $opt->show ) {
+	# get template file and add newick string
+	my $tool = lc $opt->format;
+	my $file = $ENV{'SUPERSMART_HOME'} . '/data/VISUALIZATION/' . $tool . '.tmpl';
 		
-		# make Browser::Open a 'soft' dependency; it most likely won't work on VMs and cluster computers 
-		my $bo = eval {
-			require Browser::Open;
-			1;
-		};
-		if ( $bo ) {
-			Browser::Open::open_browser( $outfile ); 
-		}
-		else {
-			$logger->warn('Browser::Open must be installed in order to show OneZoom tree in browser.')
-		}
-	}
-
-	$logger->info("DONE. Onezoom tree written to $outfile");
-
+	# instantiate template
+	my $tt = Template->new( 'ABSOLUTE' => 1 );	
+	my %args = (
+		'tree'   => $tree,
+		'width'  => $opt->width,
+		'height' => $opt->height,
+		'style'  => $opt->style,
+	);
+	my $oz = $tt->process( $file, \%args, $outfile ) or die $tt->error;
+	$logger->info("DONE. Tree written to $outfile");
 	return 1;
 }
 
