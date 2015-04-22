4737578ce3c33cc7c631e3b3fff794b7d9fc31b6
rvosa <rutgeraldo@gmail.com>
2015-04-22 17:26:10 +0200

    added supersmart drawer (+ wish list)

--- lib/Bio/SUPERSMART/App/smrtutils/Command/Plot.pm
+++ lib/Bio/SUPERSMART/App/smrtutils/Command/Plot.pm
@@ -2,6 +2,7 @@ package Bio::SUPERSMART::App::smrtutils::Command::Plot;
 use strict;
 use warnings;
 use Template;
+use Bio::Phylo::Treedrawer;
 use Bio::Phylo::IO qw(parse_tree);
 use Bio::SUPERSMART::App::SubCommand;
 use base 'Bio::SUPERSMART::App::SubCommand';
@@ -30,13 +31,14 @@ Creates a file with a graphical representation of a tree. Currently supported fo
 =cut
 
 my %formats = ( 
-	'onezoom' => 'html', 
+	'onezoom'    => 'html', 
+	'supersmart' => 'html',
 );
 
 sub options {
     
 	my ($self, $opt, $args) = @_;
-	my $format_default  = 'onezoom';
+	my $format_default  = 'supersmart';
 	my $outfile_default = "$format_default.$formats{$format_default}";
 	my $width_default   = 800;
 	my $height_default  = 600;
@@ -44,7 +46,7 @@ sub options {
 	return (
 		['tree|t=s', 'file name of input tree (newick format)', { arg => 'file', mandatory => 1 } ],
 		['outfile|o=s', "name of the output file, defaults to '$outfile_default'", { default => $outfile_default, arg => 'file' } ],	
-		['format|f=s', "output format, currently supported: OneZoom, jsPhyloSVG. Default: $format_default", { default => $format_default } ],
+		['format|f=s', "output format, currently supported: OneZoom, jsPhyloSVG, SUPERSMART. Default: $format_default", { default => $format_default } ],
 		['width|w=i',"visualization width. Default: $width_default", { default => $width_default } ],
 		['height|h=i',"visualization height. Default: $height_default", { default => $height_default } ],
 		['style|s=s',"visualization style (rectangular or circular). Default: $style_default", { default => $style_default } ],
@@ -62,6 +64,16 @@ sub validate {
 	$self->usage_error('file $file is empty') unless (-s $file);			
 }
 
+# here is my wish list for this:
+# 1. show which taxa were exemplars (bold taxon name? asterisk?)
+# 2. show the backbone branches (dashed?)
+# 3. show bootstraps, ideally as fraction, e.g. 85/100
+# 4. show posteriors, as probability 
+# 5. show higher taxa as evolving branch colors
+# 6. show branch coverage, as line thickness
+# 7. make taxa clickable (popup with link to NCBI taxonomy and to used sequences)
+# 8. make fossil nodes clickable (popup with fossil details)
+
 sub run {
 	my ($self, $opt, $args) = @_;
 	my $outfile = $opt->outfile;
@@ -73,7 +85,15 @@ sub run {
 		'-format' => 'newick',
 	);
 	
-	# get template file and add newick string
+	# compute coordinates
+	my $drawer = Bio::Phylo::Treedrawer->new(
+		'-width'  => $opt->width,
+		'-height' => $opt->height,
+		'-tree'   => $tree,
+	);
+	$drawer->compute_coordinates;
+	
+	# get template file
 	my $tool = lc $opt->format;
 	my $file = $ENV{'SUPERSMART_HOME'} . '/data/VISUALIZATION/' . $tool . '.tmpl';
 		
