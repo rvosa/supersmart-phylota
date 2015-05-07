023d6834d1d36b5fc0aba2db2d28951d98fa24f2
Rutger Vos <rutgeraldo@gmail.com>
2015-05-07 21:50:56 +0200

    adding shell script to reconcile git tags with code versions numbers [skip ci]

---
+++ conf/update_version.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# bash script to update version numbers as per 
+# issue #41 before kicking off a packer run
+
+# most recent tag version on git
+TAGVERSION=`git describe --tags | cut -f1 -d '-' | head -1`
+
+# version in framework code
+APIVERSION=`perl -I../lib -MBio::SUPERSMART -e "print $Bio::SUPERSMART::VERSION"`
+
+if [ "$TAGVERSION" -ne "$APIVERSION" ]; then
+
+	# this so that we see what's happening in the travis logs
+	echo "changing from version $APIVERSION to $TAGVERSION"
+
+	# replace in the base module file
+	sed -e "s/$APIVERSION/$TAGVERSION/" ../lib/Bio/SUPERSMART.pm > ../lib/Bio/SUPERSMART.pm.bak
+	mv ../lib/Bio/SUPERSMART.pm.bak ../lib/Bio/SUPERSMART.pm
+	
+	# replace in packer template
+	sed -e "s/$APIVERSION/$TAGVERSION/" ../conf/template.json > ../conf/template.json.bak
+	mv ../conf/template.json.bak ../conf/template.json
+else
+	echo "version unchanged: (tag) $TAGVERSION == (api) $APIVERSION"
+fi
