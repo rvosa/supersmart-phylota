6942b3aa11daabfc1dad1baf5d3120517154d9b1
Rutger Vos <rutgeraldo@gmail.com>
2015-05-14 09:38:19 +0100

    added more cleanup steps [skip ci]

--- conf/scripts/cleanup.sh
+++ conf/scripts/cleanup.sh
@@ -13,3 +13,33 @@ rm /lib/udev/rules.d/75-persistent-net-generator.rules
 
 echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
 echo "pre-up sleep 2" >> /etc/network/interfaces
+
+# ADDED ADDITIONAL CLEANUP STEPS BELOW.
+# SOURCE: http://vmassuchetto.github.io/2013/08/14/reducing-a-vagrant-box-size/
+
+# Remove APT cache
+apt-get clean -y
+apt-get autoclean -y
+
+# Remove APT files
+find /var/lib/apt -type f | xargs rm -f
+
+# Remove documentation files
+find /var/lib/doc -type f | xargs rm -f
+
+# Remove Virtualbox specific files
+rm -rf /usr/src/vboxguest* /usr/src/virtualbox-ose-guest*
+
+# Remove Linux headers
+rm -rf /usr/src/linux-headers*
+
+# Remove Unused locales (edit for your needs, this keeps only en* and pt_BR)
+find /usr/share/locale/{af,am,ar,as,ast,az,bal,be,bg,bn,bn_IN,br,bs,byn,ca,cr,cs,csb,cy,da,de,de_AT,dz,el,en_AU,en_CA,eo,es,et,et_EE,eu,fa,fi,fo,fr,fur,ga,gez,gl,gu,haw,he,hi,hr,hu,hy,id,is,it,ja,ka,kk,km,kn,ko,kok,ku,ky,lg,lt,lv,mg,mi,mk,ml,mn,mr,ms,mt,nb,ne,nl,nn,no,nso,oc,or,pa,pl,ps,qu,ro,ru,rw,si,sk,sl,so,sq,sr,sr*latin,sv,sw,ta,te,th,ti,tig,tk,tl,tr,tt,ur,urd,ve,vi,wa,wal,wo,xh,zh,zh_HK,zh_CN,zh_TW,zu} -type d -delete
+
+# Remove bash history
+unset HISTFILE
+rm -f /root/.bash_history
+rm -f /home/vagrant/.bash_history
+ 
+# Cleanup log files
+find /var/log -type f | while read f; do echo -ne '' > $f; done;
