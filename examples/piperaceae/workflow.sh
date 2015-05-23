13cd731d98121426604d98677129a3baf984517e
rvosa <rutgeraldo@gmail.com>
2015-05-23 19:05:30 +0000

    works till cladegraft

--- examples/piperaceae/workflow.sh
+++ examples/piperaceae/workflow.sh
@@ -15,7 +15,6 @@
 # this same folder.
 export NAMES=names.txt
 export FOSSILS=fossils.tsv
-export OUTGROUP=Peperomia
 
 # Step 1: match the names to the NCBI taxonomy. This is needed because ultimately all 
 # sequences that the pipeline uses are annotated with NCBI taxonomy identifiers.
@@ -29,7 +28,7 @@ smrt taxize -i $NAMES
 # - seed taxon: the identifier of the taxon to which <seed gi> belongs
 # - cluster id: the internal identifier assigned by PhyLoTA to this cluster
 # - type:       the type of cluster, usually a 'subtree' from the NCBI taxonomy
-smrt align -d alignments
+smrt align
 
 # Step 3: write the NCBI taxonomy as a classification tree. This creates a newick tree 
 # file ('classification-tree.dnd') with additional, unbranched interior nodes for all 
@@ -54,36 +53,39 @@ smrt orthologize
 #                          is too saturated to be of use.
 # - BACKBONE_MIN_COVERAGE: the minimum number of times a taxon must participate in loci
 #                          in the supermatrix in order to include it.
+# We can override these config values from the environment. As we are studying only two
+# genera we will get a backbone of only four tips, we can therefore safely build a
+# supermatrix with more coverage
+export SUPERSMART_BACKBONE_MAX_DISTANCE="0.3"
+export SUPERSMART_BACKBONE_MIN_COVERAGE="3"
 smrt bbmerge
 
 # Step 6: build a backbone tree. There are multiple inference tools that can be used for
-# this (raxml, exabayes, examl). The default is examl. The inference is done on the 
+# this (raxml, exabayes, examl). Here we use exabayes. The inference is done on the 
 # supermatrix constructed in step 5, using the classification tree from step 3 as a 
-# starting tree. In this example we will run 100 bootstrap replicates. As a result, the
-# output file ('backbone.dnd') will be a newick tree file with 100 ML trees in it. This
-# step normally creates very many intermediate files (bootstrapped matrices, various
-# log and checkpoint files produced by the inference tool, separate output tree files),
-# which we clean up by providing the '-x' flag.
-smrt bbinfer -b 100 -x
+# starting tree. In this example we follow EXABAYES defaults from supersmart.ini. As a
+# result, the output file ('backbone.dnd') will be a newick tree file a sample from the
+# posterior distribution in it. This step normally creates very many intermediate files
+# (bootstrapped matrices, various log and checkpoint files produced by the inference tool,
+# separate output tree files), which we clean up by providing the '-x' flag.
+export SUPERSMART_EXABAYES_NUMGENS="100000"
+smrt bbinfer --inferencetool=exabayes -x
 
 # Step 7: reroot the backbone trees. The trees resulting from step 6 are unrooted. There
 # are different ways to root these (using outgroups or by picking the rooting that best 
-# fits the taxonomy). We will root on the group Piper
-smrt bbreroot -g $OUTGROUP
+# fits the taxonomy). Here we fit to the taxonomy: we have two genera so this amounts to
+# the same thing as considering either of these the outgroup with respect to the other.
+# By default produces a file 'backbone-rerooted.dnd'
+smrt bbreroot --smooth
 
 # Step 8: calibrate the backbone trees from step 7. This step uses treePL to create 
 # ultrametric trees ('chronogram.dnd') using a penalized likelihood approach with 
 # age constraints as extracted from the fossils file.
 smrt bbcalibrate -f $FOSSILS
 
-# Step 9: build a consensus tree. In this example we have 100 bootstrap trees that we have
-# rerooted and calibrated. By now building a consensus we will end up with a single tree
-# that will have bootstrap values and bootstrapped age ranges on the branches. The 
-# consensus is calculated using treeannotator, and so the metadata that is attached to the
-# nodes can be viewed by opening the resulting file ('consensus.nex') in figtree. As the 
-# trees are bootstrap trees we don't throw out a burnin fraction. This would be different 
-# had we used exabayes in step 6.
-smrt consense --burnin=0
+# Step 9: build a consensus tree. As we ran exabayes in step 6 we will want to discard
+# a burnin. This would be different had we done a bootstrapping analysis in step 6.
+smrt consense --burnin=0.20
 
 # Step 10: decompose the backbone into clades. This step traverses the consensus tree from
 # step 9 and breaks it up into monophyletic clades (in principle, genera, unless these are
@@ -92,6 +94,8 @@ smrt consense --burnin=0
 # 2) are selected that meet the following criteria:
 # - CLADE_MAX_DISTANCE: maximum average pairwise distance to accept the alignment
 # - CLADE_MIN_DENSITY:  minimum number of clade members that must be present
+export SUPERSMART_CLADE_MAX_DISTANCE="0.3"
+export SUPERSMART_CLADE_MIN_DENSITY="0.5"
 smrt bbdecompose
 
 # Step 11: for each clade, merge the separate clade alignments from step 10 into a single
@@ -103,8 +107,8 @@ smrt clademerge
 # make any general recommendations for what the right number is because this depends on a
 # lot of different variables (number of taxa, signal in the data, mixing of the chains)
 # so for publishable results convergence should be checked, for example using 'tracer'.
-# For this specific example, 30,000,000 generations for each clade appears to work.
-smrt cladeinfer -n 30_000_000
+# For this specific example, 10,000,000 generations for each clade appears to work.
+smrt cladeinfer -n 10_000_000
 
 # Step 13: graft the clade trees onto the backbone.
 # XXX ANNOTATIONS LOST WHEN READING IN CONSENSE TREE!
