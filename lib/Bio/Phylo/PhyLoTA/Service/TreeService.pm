64adcb5c6866ada44191d30a0ec44244df8451ea
rvosa <rutgeraldo@gmail.com>
2015-06-10 12:31:49 +0000

    clade names now attached as 'fig:clade' annotations [skip ci]

--- lib/Bio/Phylo/PhyLoTA/Service/TreeService.pm
+++ lib/Bio/Phylo/PhyLoTA/Service/TreeService.pm
@@ -7,6 +7,7 @@ use Bio::Phylo::IO qw'parse parse_tree unparse';
 use Bio::Phylo::Factory;
 use Bio::Phylo::PhyLoTA::Config;
 use Bio::Phylo::PhyLoTA::Service;
+use Bio::Phylo::Util::CONSTANT ':namespaces';
 use base 'Bio::Phylo::PhyLoTA::Service';
 use Bio::Phylo::PhyLoTA::Domain::MarkersAndTaxa;
 use Bio::Phylo::PhyLoTA::Service::MarkersAndTaxaSelector;
@@ -791,7 +792,13 @@ sub graft_tree {
 	my $branch_length = $bmrca->get_branch_length || 0;
  	$bmrca->set_branch_length( $branch_length + $diff ); # XXX still gives negative branches
 	$log->debug("adjusted backbone MRCA depth by $bmrca_depth - $crd");
-    
+
+	# copy the clade annotation, if any
+	if ( my $value = $clade->get_root->get_meta_object('fig:clade') ) {
+		$bmrca->set_namespaces( 'fig' => _NS_FIGTREE_ );
+		$bmrca->set_meta_object( 'fig:clade' => $value );
+	}
+
 	# now graft!
  	$bmrca->clear();
  	$clade->visit(sub {
