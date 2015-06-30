55af021169a9592737b13dd6168fc79c7a239604
hettling <hannes.hettling@gmail.com>
2015-06-30 17:41:35 +0200

    [skip ci] workflow for quails with simulationswork now

--- examples/quails-sim/workflow.sh
+++ examples/quails-sim/workflow.sh
@@ -5,17 +5,20 @@ smrt taxize -r Coturnix,Odontophoridae
 smrt align
 smrt classify
 smrt orthologize
-export SUPERSMART_BACKBONE_MAX_DISTANCE="0.2"
-export SUPERSMART_BACKBONE_MIN_COVERAGE="3"
-smrt bbmerge
+export SUPERSMART_BACKBONE_MAX_DISTANCE=0.2
+export SUPERSMART_BACKBONE_MIN_COVERAGE=3
+smrt bbmerge -v
 smrt bbinfer --cleanup
 smrt bbreroot --smooth
 smrt bbcalibrate --fossiltable=$FOSSILS
 smrt consense --burnin=0.20 --prob
-export SUPERSMART_CLADE_MAX_DISTANCE="0.1"
-export SUPERSMART_CLADE_MIN_DENSITY="0.3"
+export SUPERSMART_CLADE_MAX_DISTANCE=0.3
+export SUPERSMART_CLADE_MIN_DENSITY=0.2
+export SUPERSMART_CLADE_MAX_MARKERS=10
+export SUPERSMART_CLADE_TAXON_MIN_MARKERS=2
+export SUPERSMART_CLADE_MAX_HAPLOTYPES=3
 smrt bbdecompose
-smrt clademerge --enrich
+smrt clademerge 
 smrt cladeinfer --ngens=100_000 --sfreq=100 --lfreq=100
 smrt cladegraft
 
