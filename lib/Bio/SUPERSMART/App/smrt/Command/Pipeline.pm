1b14954ba51b64876e89bc3601ac9c043331b340
hettling <hannes.hettling@gmail.com>
2015-08-11 17:55:01 +0200

    [skip ci] removed smrt bbcalibrate

--- lib/Bio/SUPERSMART/App/smrt/Command/Pipeline.pm
+++ lib/Bio/SUPERSMART/App/smrt/Command/Pipeline.pm
@@ -74,7 +74,7 @@ sub run{
 		my ($taxon) = split(',', $root_taxa);				
 		require POSIX; 
 		require Cwd;
-		my $date = POSIX::strftime "%m-%d-%Y", localtime;		
+		my $date = POSIX::strftime "%Y-%m-%d", localtime;		
 		$workdir = Cwd::getcwd . '/' . $date . '-' . $taxon;
 		$self->workdir($workdir);
 		$log->info("No working directory given in option. Creating working directory $workdir");
@@ -82,68 +82,59 @@ sub run{
 	}
 	
 	# define file names for intermediate files
-	my $taxafile = $self->absolute_path("taxa.tsv");
-	my $classtreefile = $self->absolute_path("classtree.dnd");
+	my $taxafile = $self->absolute_path("species.tsv");
 	my $alnfile = $self->absolute_path("aligned.txt");
 	my $orthfile = $self->absolute_path("clusters.txt");
 	my $supermatrixfile = $self->absolute_path("supermatrix.phy");
 	my $backbonefile = $self->absolute_path("backbone.dnd");
 	my $rerootedfile = $self->absolute_path("backbone-rerooted.dnd");
 	my $chronogramfile = $self->absolute_path("chronogram.dnd");
-	my $finaltreefile = $self->absolute_path("final.dnd");
+	my $consensefile = $self->absolute_path("consense.nex");
+	my $finaltreefile = $self->absolute_path("final.nex");
 	
 	# other parameters
 	my $beast_gens = 100000;
 	
 	# Step 1: Taxize 
 	$log->info("Running pipeline step #1: smrt taxize");
-	my $success = $self->_run_subcommand( ("taxize",  "-w", $workdir, "-r", $root_taxa, "-o", $taxafile, "-e", "Species", "-w", $workdir, "-l", "taxize.log") );	
+	my $success = $self->_run_subcommand( ("taxize",  "-w", $workdir, "-r", $root_taxa, "-o", $taxafile, "-e", "Subspecies", "-w", $workdir, "-l", "taxize.log") );	
 	die ("Pipeline step 'taxize' failed. Try to run 'smrt taxize' manually with '-v' option to find out what went wrong.") unless ( $success and  -e $taxafile and -s $taxafile);
 	$log->info("Step #1 smrt taxize succeed");
-	
-	# Step 2: Classify
-	$log->info("Running pipeline step #2: smrt classify");
-	$success = $self->_run_subcommand( ("classify",  "-w", $workdir, "-i", $taxafile, "-o", $classtreefile, "-l", "classify.log") );	
-	die ("Pipeline step 'classify' failed. Try to run 'smrt classify' manually with '-v' option to find out what went wrong.") unless ( $success and  -e $classtreefile and -s $classtreefile);
-	$log->info("Step #2 smrt classify succeed");
-	
-	# Step 3: Align
+		
+	# Step 2: Align
 	$log->info("Running pipeline step #3: smrt align");
 	$success = $self->_run_subcommand( ("align",  "-w", $workdir, "-i", $taxafile, "-o", $alnfile, "-l", "align.log") );	
 	die ("Pipeline step 'align' failed. Try to run 'smrt align' manually with '-v' option to find out what went wrong.") unless ( $success and  -e $alnfile and -s $alnfile);
 	$log->info("Step #3 smrt align succeed");
 	
-	# Step 4: Orthologize
+	# Step 3: Orthologize
 	$log->info("Running pipeline step #4: smrt orthologize");
 	$success = $self->_run_subcommand( ("orthologize",  "-w", $workdir, "-i", $alnfile, "-o", $orthfile, "-l", "orthologize.log") );	
 	die ("Pipeline step 'orthologize' failed. Try to run 'smrt orthologize' manually with '-v' option to find out what went wrong.") unless ( $success and  -e $orthfile and -s $orthfile);
 	$log->info("Step #4 smrt orthologize succeed");
 	
-	# Step 5: BBmerge
+	# Step 4: BBmerge
 	$log->info("Running pipeline step #5: smrt bbmerge");
 	$success = $self->_run_subcommand( ("bbmerge",  "-w", $workdir, "-a", $orthfile, "-t", $taxafile, "-o", $supermatrixfile, "-l", "bbmerge.log") );	
 	die ("Pipeline step 'bbmerge' failed. Try to run 'smrt bbmerge' manually with '-v' option to find out what went wrong.") unless ( $success and  -e $supermatrixfile and -s $supermatrixfile);
 	$log->info("Step #5 smrt bbmerge succeed");
 		
-	# Step 6: BBinfer
+	# Step 5: BBinfer
 	$log->info("Running pipeline step #6: smrt bbinfer");
 	
-	$success = $self->_run_subcommand( ("bbinfer",  "-w", $workdir, "-s", $supermatrixfile, "-t", $classtreefile, "-o", $backbonefile, "-i", "examl", "-l", "bbinfer.log") );	
+	$success = $self->_run_subcommand( ("bbinfer",  "-w", $workdir, "-s", $supermatrixfile, "-t", $taxafile, "-o", $backbonefile, "-i", "examl", "-b", 100, "-l", "bbinfer.log") );	
 	die ("Pipeline step 'bbinfer' failed. Try to run 'smrt bbinfer' manually with '-v' option to find out what went wrong.") unless ( $success and  -e $backbonefile and -s $backbonefile);
 	$log->info("Step #6 smrt bbinfer succeed");
 
-	# Step 7: BBreroot
+	# Step 6: BBreroot
 	$log->info("Running pipeline step #7: smrt bbreroot");
-	if ( $outgroup ) {
-		$success = $self->_run_subcommand( ("bbreroot",  "-w", $workdir, "-b", $backbonefile, "-t", $taxafile, "-o", $rerootedfile, "-g", $outgroup, "-l", "bbreroot.log") );	
-	}
-	else {
-		$success = $self->_run_subcommand( ("bbreroot",  "-w", $workdir, "-b", $backbonefile, "-t", $taxafile, "-o", $rerootedfile, "-l", "bbreroot.log") );			
-	}
+	my @params = (("bbreroot",  "-w", $workdir, "-b", $backbonefile, "-t", $taxafile, "-o", $rerootedfile, "-l", "bbreroot.log", '-v'));
+	push @params, ("-g",  $outgroup) if $outgroup;
+	$success = $self->_run_subcommand( ("bbreroot",  "-w", $workdir, "-b", $backbonefile, "-t", $taxafile, "-o", $rerootedfile, "-l", "bbreroot.log") );				
 	die ("Pipeline step 'bbreroot' failed. Try to run 'smrt bbreroot' manually with '-v' option to find out what went wrong.") unless ( $success and  -e $rerootedfile and -s $rerootedfile);
 	$log->info("Step #7 smrt bbreroot succeed");
 	
-	# Step 8: BBcalibrate
+	# Step 7: BBcalibrate
 	if ( my $fossilfile = $opt->fossiltable ) {
 		$log->info("Running pipeline step #8: smrt bbcalibrate");
 		$success = $self->_run_subcommand( ("bbcalibrate",  "-w", $workdir, "-s", $supermatrixfile, "-t", $rerootedfile, "-o", $chronogramfile, "-f", $fossilfile, "-l", "bbcalibrate.log") );	
@@ -155,28 +146,31 @@ sub run{
 		$chronogramfile = $backbonefile;
 	}
 	
-	# Step 9: BBdecompose
+	# Step 8: Consense
+	$log->info("Running pipeline step #8: smrt consense");
+	$success = $self->_run_subcommand( ("consense",  "-w", $workdir, "-t", $chronogramfile, "-o", $consensefile, "-l", "-b", 0.0, "consense.log") );	
+
+	# Step 8: BBdecompose
 	$log->info("Running pipeline step #9: smrt bbdecompose");
-	$success = $self->_run_subcommand( ("bbdecompose",  "-w", $workdir, "-b", $chronogramfile, "-c", $classtreefile, "-a", $alnfile, "-t", $taxafile, "-l", "bbdecompose.log") );	
+	$success = $self->_run_subcommand( ("bbdecompose",  "-w", $workdir, "-b", $consensefile, "-a", $alnfile, "-t", $taxafile, "-l", "bbdecompose.log") );	
 	die ("Pipeline step 'bbdecompose' failed. Try to run 'smrt bbdecompose' manually with '-v' option to find out what went wrong.") unless ( $success );
 	$log->info("Step #9 smrt bbdecompose succeed");
 	
-	
-	# Step 8: Clademerge
+	# Step 9: Clademerge
 	$log->info("Running pipeline step #10: smrt clademerge");
 	$success = $self->_run_subcommand( ("clademerge",  "-w", $workdir, "-l", "clademerge.log") );	
 	die ("Pipeline step 'clademerge' failed. Try to run 'smrt clademerge' manually with '-v' option to find out what went wrong.") unless $success;
 	$log->info("Step #10 smrt clademerge succeed");
 		
-	# Step 9: Cladeinfer
+	# Step 10: Cladeinfer
 	$log->info("Running pipeline step #11: smrt cladeinfer");
 	$success = $self->_run_subcommand( ("cladeinfer",  "-w", $workdir, "-n", $beast_gens, "-l", "cladeinfer.log") );	
 	die ("Pipeline step 'bbinfer' failed. Try to run 'smrt bbinfer' manually with '-v' option to find out what went wrong.") unless $success;
 	$log->info("Step #11 smrt cladeinfer succeed");
 
-	# Step 10: Cladegraft
+	# Step 11: Cladegraft
 	$log->info("Running pipeline step #12: smrt cladegraft");
-	$success = $self->_run_subcommand( ("cladegraft",  "-w", $workdir, "-b", $chronogramfile, "-o", $finaltreefile, "-l", "cladegraft.log") );	
+	$success = $self->_run_subcommand( ("cladegraft",  "-w", $workdir, "-b", $consensefile, "-o", $finaltreefile, "-l", "cladegraft.log") );	
 	die ("Pipeline step 'cladegraft' failed. Try to run 'smrt cladegraft' manually with '-v' option to find out what went wrong.") unless ( $success and  -e $finaltreefile and -s $finaltreefile);
 	$log->info("Final step #12 smrt cladegraft succeed. Final file written to $finaltreefile");
 
