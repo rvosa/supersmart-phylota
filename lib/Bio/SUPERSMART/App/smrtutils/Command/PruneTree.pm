7602676f6cb77e02fa81c5ca053d031bf9947d73
hettling <hannes.hettling@gmail.com>
2015-11-15 22:34:07 +0100

    [skip ci] added treeformat option

--- lib/Bio/SUPERSMART/App/smrtutils/Command/PruneTree.pm
+++ lib/Bio/SUPERSMART/App/smrtutils/Command/PruneTree.pm
@@ -6,7 +6,7 @@ use warnings;
 use Bio::Phylo::PhyLoTA::Service::TreeService;
 use Bio::Phylo::PhyLoTA::Service::MarkersAndTaxaSelector;
 
-use Bio::Phylo::IO qw(parse);
+use Bio::Phylo::IO qw(parse_tree);
 
 use base 'Bio::SUPERSMART::App::SubCommand';
 use Bio::SUPERSMART::App::smrtutils qw(-command);
@@ -23,54 +23,55 @@ smrt-utils prunetree [-h ] [-v ] [-w <dir>] [-l <file>] [-y ] -t <file> [-o <fil
 
 =head1 DESCRIPTION
 
-Given an input tree and one or more taxa, prunes out the taxa from the tree. Input taxa of higher level are 
+Given an input tree and one or more taxa, prunes out the taxa from the tree. Input taxa of higher level are
 expanded and all species (or lower) belonging to the higher level taxon are pruned out (if present in the tree).
 
 =cut
 
 sub options {
-    
-	my ($self, $opt, $args) = @_;		
+
+	my ($self, $opt, $args) = @_;
 	my $outfile_default = 'pruned.dnd';
+	my $format_default = 'nexus';
 	return (
-		['tree|t=s', 'file name of input tree (newick format)', { arg => 'file', mandatory => 1}],
-		['outfile|o=s', "name of the output file, defaults to '$outfile_default'", {default => $outfile_default, arg => 'file'}],	
-		['taxa|g=s', 'one or multiple taxa to be pruned from the tree', {} ],		    
+		['tree|t=s', 'file name of input tree', { arg => 'file', mandatory => 1}],
+		['tree_format|f=s', "format of tree input file (newick or nexus), defaults to $format_default", { default => $format_default } ],
+		['outfile|o=s', "name of the output file, defaults to '$outfile_default'", {default => $outfile_default, arg => 'file'}],
+		['taxa|g=s', 'one or multiple taxa to be pruned from the tree', {} ],
 		['negative_branches|n', 'prune out branches (and appendent nodes) which have negative length']
-	    );	
+	    );
 }
 
 sub validate {
-	my ($self, $opt, $args) = @_;		
-	
-	# We only have to check the 'infile' argument. 
-	#  If the infile is absent or empty, abort  
+	my ($self, $opt, $args) = @_;
+
+	# We only have to check the 'infile' argument.
+	#  If the infile is absent or empty, abort
 	my $file = $opt->tree;
 	$self->usage_error('no tree argument given') if not $file;
 	$self->usage_error('file $file does not exist') unless (-e $file);
-	$self->usage_error('file $file is empty') unless (-s $file);			
+	$self->usage_error('file $file is empty') unless (-s $file);
 	$self->usage_error('taxa and/or negative_branches flag must be given') if not ($opt->taxa or $opt->negative_branches);
 }
 
 sub run {
-	my ($self, $opt, $args) = @_;    
+	my ($self, $opt, $args) = @_;
 	my $treefile = $opt->tree;
 	my $taxa = $opt->taxa;
 	my $outfile = $opt->outfile;
 	my $logger = $self->logger;
-	
+
 	my $ts = Bio::Phylo::PhyLoTA::Service::TreeService->new;
 	my $mts = Bio::Phylo::PhyLoTA::Service::MarkersAndTaxaSelector->new;
-	
-	my $tree = parse(
+
+	my $tree = parse_tree(
 		'-file'   => $treefile,
-		'-format' => 'newick',
-	    )->first;
-	
+		'-format' => $opt->tree_format,
+	    );
 	my $ntax_initial = $tree->get_ntax;
-	
-	if ( $taxa ) {		    
-		my @names = split(',', $taxa);    
+
+	if ( $taxa ) {
+		my @names = split(',', $taxa);
 		for my $n (@names) {
 			# taxa to prune could be higher level, so search all child taxa until lowest rank
 			my %all_taxa = map {$_=~s/_/|/g; $_=~s/\ /_/g; $_=~s/\'|\"//g;  $_=>1} $mts->expand_taxa([$n], 'Forma');
@@ -85,13 +86,13 @@ sub run {
 				}
 			}
 			# prune
-			$logger->info('Pruning ' . scalar (@to_prune) . " terminals from tree for taxon $n");				    
+			$logger->info('Pruning ' . scalar (@to_prune) . " terminals from tree for taxon $n");
 			$tree->prune_tips(\@to_prune);
 		}
-	}      
+	}
 	if ( $opt->negative_branches ) {
 		my $count = 0;
-		$tree->visit(sub{ 
+		$tree->visit(sub{
 			my $node = shift;
 			my $bl = $node->get_branch_length || 0;
 			if ( $bl < 0 ) {
@@ -107,12 +108,12 @@ sub run {
 
 	my $ntax_pruned = $tree->get_ntax;
 	$logger->info("Taxon number changed from $ntax_initial to $ntax_pruned");
-	
+
 	# write rerooted tree to output file
-	open my $out, '>', $outfile or die $!;	
+	open my $out, '>', $outfile or die $!;
 	print $out $tree->to_newick(nodelabels=>1);
 	close $out;
-	
+
 	$logger->info("DONE. Outfile written to $outfile");
 	return 1;
 }
