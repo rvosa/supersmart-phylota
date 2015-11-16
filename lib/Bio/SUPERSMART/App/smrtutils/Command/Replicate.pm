80ee7ebd8b9f78dca5bf92747c149137d5d27fbf
hettling <hannes.hettling@gmail.com>
2015-11-16 08:43:15 +0100

    [skip ci] changed Replicate.pm

--- lib/Bio/SUPERSMART/App/smrtutils/Command/Replicate.pm
+++ lib/Bio/SUPERSMART/App/smrtutils/Command/Replicate.pm
@@ -288,7 +288,7 @@ sub _replicate_alignment {
 
 	# determine substitution model for given alignment
 	my $timeout = 1800; # set to 30 min
-	my $model = 'Bio::Phylo::Models::Substitution::Dna'->modeltest( '-matrix' => $matrix, '-timeout' => $timeout, '-tree' => $original_tree );
+	my $model = 'Bio::Phylo::Models::Substitution::Dna'->modeltest( '-matrix' => $matrix, '-timeout' => $timeout );
 
 	# prune tree for faster sequence simulations
 	my $pruned = parse('-format'=>'newick', '-string'=>$tree->to_newick)->first;
